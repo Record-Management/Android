f345fc6f5fe07ab84b5773750c5c7a140d8e14d4
이건희 <leech9876@naver.com>
2025-07-21 10:24:25 +0900

    [ANDROID-5] feat : Timber 커스텀클래스 작성
    팀버를 사용해서 클래스나 라인 정보를 편하게 얻자

---
+++ app/src/main/java/record/daily/android/timber/ClassNameCustomTimber.kt
@@ -0,0 +1,9 @@
+package record.daily.android.timber
+
+import timber.log.Timber
+
+class ClassNameCustomTimber : Timber.DebugTree() {
+
+    override fun createStackElementTag(element: StackTraceElement): String = "${element.fileName}:${element.lineNumber}#${element.methodName}"
+
+}
