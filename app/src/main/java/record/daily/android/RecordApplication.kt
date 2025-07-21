57abe6fc26030d43e8efe9fabe7f38d1d3d978e6
이건희 <leech9876@naver.com>
2025-07-21 10:26:00 +0900

    [ANDROID-5] feat : Application에 Timber 등록

--- app/src/main/java/record/daily/android/RecordApplication.kt
+++ app/src/main/java/record/daily/android/RecordApplication.kt
@@ -2,7 +2,17 @@ package record.daily.android
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import record.daily.android.timber.ClassNameCustomTimber
+import timber.log.Timber
 
 @HiltAndroidApp
 class RecordApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if(BuildConfig.DEBUG) {
+            Timber.plant(ClassNameCustomTimber())
+        }
+    }
 }
