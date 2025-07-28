146839369f0abcde9a14e178052825814e1e2119
이건희 <leech9876@naver.com>
2025-07-28 20:37:00 +0900

    [MBA-10] feat : 스플래시 화면에서 앱이 죽는 이슈 수정

--- app/src/main/java/record/daily/android/MainActivity.kt
+++ app/src/main/java/record/daily/android/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        // 이슈 해결
         setContent {
             AndroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
