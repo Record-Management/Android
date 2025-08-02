8eba01ba5b517b0beaf301822b7158877cfb6b5f
이건희 <leech9876@naver.com>
2025-08-02 09:51:02 +0900

    [ANDROID-6] feat : LoginActivity로 이동하는 코드 작성

--- app/src/main/java/record/daily/android/MainActivity.kt
+++ app/src/main/java/record/daily/android/MainActivity.kt
@@ -1,29 +1,40 @@
 package record.daily.android
 
+import android.content.Intent
 import android.os.Bundle
+import android.service.autofill.OnClickAction
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import record.daily.android.ui.theme.AndroidTheme
+import record.daily.login.LoginActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        // 이슈 해결
+
+        val onClickLoginActivity: () -> Unit = {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
         setContent {
             AndroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier.padding(innerPadding),
+                        onClickLogin = onClickLoginActivity,
                     )
                 }
             }
@@ -32,17 +43,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(
+    name: String,
+    modifier: Modifier = Modifier,
+    onClickLogin: () -> Unit,
+) {
+    Column {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        Button(onClickLogin) {
+            Text("로그인 화면으로 이동")
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AndroidTheme {
-        Greeting("Android")
+        Greeting("Android", onClickLogin = {})
     }
 }
