3b582dba8b53f738a444d09db2d2a4c8091d40dd
이건희 <leech9876@naver.com>
2025-09-01 15:38:43 +0900

    [ANDROID-19] build : okhttp, retrofit, kotlinx 의존성 추가
    네트워크 통신을 하기 위해 추가했다

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("youth.android.library")
+    id("kotlinx-serialization")
+    kotlin("plugin.serialization")
 }
 
 android {
@@ -8,4 +10,12 @@ android {
 
 dependencies {
     implementation(project(":core:domain"))
+
+    implementation(libs.retrofit)
+    implementation(libs.okhttp)
+    implementation(platform(libs.okhttp.bom))
+    implementation(libs.okhttp3.okhttp)
+    implementation(libs.logging.interceptor)
+    implementation(libs.retrofit.kotlin.serialization)
+    implementation(libs.kotlinx.serialization.json)
 }
