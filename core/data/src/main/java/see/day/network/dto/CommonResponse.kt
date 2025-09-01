7f761475df727e1c3ec250a4f877c3c1cb2a5701
이건희 <leech9876@naver.com>
2025-09-01 23:48:55 +0900

    [ANDROID-20] feat : 공통 응답 객체 작성

---
+++ core/data/src/main/java/see/day/network/dto/CommonResponse.kt
@@ -0,0 +1,19 @@
+package see.day.network.dto
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.serializer
+import okhttp3.ResponseBody.Companion.toResponseBody
+
+@Serializable
+data class CommonResponse<T>(
+    val statusCode: Int,
+    val code: String,
+    val message: String,
+    val data: T?
+)
+
+inline fun <reified T> toResponseBody(request: CommonResponse<T>) = Json.encodeToString(
+    serializer<CommonResponse<T>>(),
+    request
+).toResponseBody()
