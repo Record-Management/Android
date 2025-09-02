c65ab5ddb6a8e7c6738c49ce91c5e08771f68946
이건희 <leech9876@naver.com>
2025-09-02 14:19:47 +0900

    [ANDROID-20] feat : 공통응답에 로그인 응답값을 넣는 방식으로 변경

--- core/data/src/main/java/see/day/network/LoginService.kt
+++ core/data/src/main/java/see/day/network/LoginService.kt
@@ -3,10 +3,11 @@ package see.day.network
 import okhttp3.RequestBody
 import retrofit2.http.Body
 import retrofit2.http.POST
+import see.day.network.dto.CommonResponse
 import see.day.network.dto.login.LoginResponse
 
 interface LoginService {
 
     @POST("api/auth/social-login")
-    suspend fun signIn(@Body requestBody: RequestBody): LoginResponse
+    suspend fun signIn(@Body requestBody: RequestBody): CommonResponse<LoginResponse>
 }
