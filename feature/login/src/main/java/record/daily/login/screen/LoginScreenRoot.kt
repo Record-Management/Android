adea394accb3be37c6dd73af38e4dd3f04705061
이건희 <leech9876@naver.com>
2025-09-05 00:03:52 +0900

    [ANDROID-25] feat : 로그인 화면 작성
    비즈니스 로직은 추후 추가 API도 수정해야되는 부분이 많음

--- feature/login/src/main/java/record/daily/login/screen/LoginScreenRoot.kt
+++ feature/login/src/main/java/record/daily/login/screen/LoginScreenRoot.kt
@@ -1,13 +1,20 @@
 package record.daily.login.screen
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import record.daily.login.component.KakaoLogin
+import record.daily.login.component.SplashScreen
 import record.daily.login.social.kakaoLogin
+import see.day.designsystem.theme.SeeDayTheme
 import timber.log.Timber
 
 @Composable
@@ -28,19 +35,25 @@ internal fun LoginScreenRoot(modifier: Modifier = Modifier) {
 internal fun LoginScreen(modifier: Modifier = Modifier, onClickKakaoLogin: () -> Unit) {
     Column(
         modifier = modifier
+            .fillMaxSize()
+            .systemBarsPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("hello LoginScreen")
-
-        Button(onClickKakaoLogin) {
-            Text("Kakao Login")
-        }
+        SplashScreen(modifier = modifier)
+        KakaoLogin(
+            modifier = modifier,
+            onClickKakaoLogin = onClickKakaoLogin
+        )
+        Spacer(modifier = modifier.padding(bottom = 24.dp))
     }
 }
 
 @Preview
 @Composable
 private fun LoginScreenPreview() {
-    LoginScreen(
-        onClickKakaoLogin = {}
-    )
+    SeeDayTheme {
+        LoginScreen(
+            onClickKakaoLogin = {}
+        )
+    }
 }
