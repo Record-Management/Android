20d12cefd837664e890829ee15ca0246d136bba7
이건희 <leech9876@naver.com>
2025-09-05 11:17:23 +0900

    [ANDROID-27] feat : 각 모듈별로 Navigation 작성

---
+++ feature/login/src/main/java/record/daily/login/navigation/LoginNavigation.kt
@@ -0,0 +1,19 @@
+package record.daily.login.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import record.daily.login.screen.LoginScreenRoot
+
+const val LOGIN_ROUTE = "LOGIN"
+
+fun NavController.navigateLogin(navOptions: NavOptions? = null) {
+    navigate(LOGIN_ROUTE, navOptions)
+}
+
+fun NavGraphBuilder.loginNavigation() {
+    composable(LOGIN_ROUTE) {
+        LoginScreenRoot()
+    }
+}
