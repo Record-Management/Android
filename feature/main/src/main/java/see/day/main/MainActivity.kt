6f0618ef4e7d9523510671d4f3f8654582c634cb
이건희 <leech9876@naver.com>
2025-09-05 11:18:44 +0900

    [ANDROID-27] feat : MainActivity에서 액티비티로 이동하는 로직 삭제

--- feature/main/src/main/java/see/day/main/MainActivity.kt
+++ feature/main/src/main/java/see/day/main/MainActivity.kt
@@ -1,41 +1,25 @@
 package see.day.main
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import record.daily.login.LoginActivity
 import see.day.designsystem.theme.SeeDayTheme
+import see.day.main.navigation.SeedayApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val onClickLoginActivity: () -> Unit = {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
-
         setContent {
             SeeDayTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding),
-                        onClickLogin = onClickLoginActivity
-                    )
-                }
+                SeedayApp()
             }
         }
     }
@@ -51,13 +35,5 @@ fun Greeting(name: String, modifier: Modifier = Modifier, onClickLogin: () -> Un
         Button(onClickLogin) {
             Text("로그인 화면으로 이동")
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SeeDayTheme {
-        Greeting("Android", onClickLogin = {})
     }
 }
