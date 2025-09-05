8d29da4bde3846c5107836a8566e3146c2556780
이건희 <leech9876@naver.com>
2025-09-05 11:18:25 +0900

    [ANDROID-27] feat : App 컴포저블 함수와 NavigationState작성
    Navigation 관리의 편의성을 위해 사용한다

---
+++ feature/main/src/main/java/see/day/main/navigation/SeedayApp.kt
@@ -0,0 +1,32 @@
+package see.day.main.navigation
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import record.daily.login.navigation.loginNavigation
+import see.day.main.navigation.graph.MAIN_ROUTE
+import see.day.main.navigation.graph.mainNavigation
+
+@Composable
+fun SeedayApp(navigationState: NavigationState = rememberNavigationState()) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        NavHost(navigationState.navController, startDestination = MAIN_ROUTE) {
+            mainNavigation(
+                onClickLogin = navigationState::navigateLogin
+            )
+            loginNavigation()
+        }
+    }
+}
+
+@Composable
+fun rememberNavigationState(navController: NavHostController = rememberNavController()): NavigationState {
+    return remember(navController) {
+        NavigationState(navController)
+    }
+}
