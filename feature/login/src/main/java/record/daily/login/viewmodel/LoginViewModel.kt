8f0fc1daf9a3cf940684a1a06bfdff10ff89faa0
이건희 <leech9876@naver.com>
2025-09-06 20:24:06 +0900

    [ANDROID-30] feat : LoginViewModel, UiEffect 작성
    UiState나 UiEvent는 필요 없으므로 이것들만 설정

---
+++ feature/login/src/main/java/record/daily/login/viewmodel/LoginViewModel.kt
@@ -0,0 +1,38 @@
+package record.daily.login.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import record.daily.login.state.login.LoginUiEffect
+import see.day.domain.usecase.login.PostLoginUseCase
+import see.day.model.login.SocialLogin
+import see.day.model.login.SocialType
+import see.day.model.navigation.AppStartState.MAIN
+import see.day.model.navigation.AppStartState.ONBOARDING
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    private val postLoginUseCase: PostLoginUseCase
+) : ViewModel() {
+
+    private val _uiEffect: MutableSharedFlow<LoginUiEffect> = MutableSharedFlow()
+    val uiEffect: SharedFlow<LoginUiEffect> = _uiEffect.asSharedFlow()
+
+    fun login(socialType: SocialType, accessToken: String) {
+        viewModelScope.launch {
+            postLoginUseCase(SocialLogin(socialType, accessToken)).onSuccess {
+                if (it == ONBOARDING) {
+                    _uiEffect.emit(LoginUiEffect.GoOnboarding)
+                } else if (it == MAIN) {
+                    _uiEffect.emit(LoginUiEffect.GoMain)
+                }
+            }.onFailure {
+            }
+        }
+    }
+}
