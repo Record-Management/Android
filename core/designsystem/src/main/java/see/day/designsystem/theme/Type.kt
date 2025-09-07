c2f135a27918eb39d0ce52e53325f6a360f92cb2
이건희 <leech9876@naver.com>
2025-09-07 16:10:01 +0900

    [ANDROID-13] style : Font 적용

--- core/designsystem/src/main/java/see/day/designsystem/theme/Type.kt
+++ core/designsystem/src/main/java/see/day/designsystem/theme/Type.kt
@@ -2,33 +2,125 @@ package see.day.designsystem.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import see.day.designsystem.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+val preTendFont =
+    FontFamily(
+        Font(R.font.pretendard_regular, FontWeight.Normal, FontStyle.Normal),
+        Font(R.font.pretendard_bold, FontWeight.Bold, FontStyle.Normal),
+        Font(R.font.pretendard_semibold, FontWeight.SemiBold, FontStyle.Normal)
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+
+val Typography =
+    Typography(
+        // P/22/Bold
+        bodyLarge =
+            TextStyle(
+                fontFamily = preTendFont,
+                fontWeight = FontWeight.W700,
+                fontSize = 22.sp,
+                color = gray100
+            ),
+        // P/20/Bold
+        bodyMedium =
+            TextStyle(
+                fontFamily = preTendFont,
+                fontWeight = FontWeight.W700,
+                fontSize = 20.sp,
+                color = gray100
+            ),
+        // P/20/SemiBold
+        bodySmall =
+            TextStyle(
+                fontFamily = preTendFont,
+                fontWeight = FontWeight.W600,
+                fontSize = 20.sp,
+                color = gray100
+            ),
+        // P/18/SemiBold
+        titleLarge =
+            TextStyle(
+                fontFamily = preTendFont,
+                fontWeight = FontWeight.W600,
+                fontSize = 18.sp,
+                color = gray100
+            ),
+        // P/18/Medium
+        titleMedium =
+            TextStyle(
+                fontFamily = preTendFont,
+                fontWeight = FontWeight.W500,
+                fontSize = 18.sp,
+                color = gray100
+            ),
+        // P/16/SemiBold
+        titleSmall =
+            TextStyle(
+                fontFamily = preTendFont,
+                fontWeight = FontWeight.W600,
+                fontSize = 16.sp,
+                color = gray100
+            ),
+        // P/16/Medium
+        displayLarge = TextStyle(
+            fontFamily = preTendFont,
+            fontWeight = FontWeight.W500,
+            fontSize = 16.sp,
+            color = gray100
+        ),
+        // P/16/Regular
+        displayMedium =
+            TextStyle(
+                fontFamily = preTendFont,
+                fontWeight = FontWeight.W400,
+                fontSize = 16.sp,
+                color = gray100
+            ),
+        // P/14/Semibold
+        displaySmall = TextStyle(
+            fontFamily = preTendFont,
+            fontWeight = FontWeight.W600,
+            fontSize = 14.sp,
+            color = gray100
+        ),
+        // P/14/Medium
+        labelMedium = TextStyle(
+            fontFamily = preTendFont,
+            fontWeight = FontWeight.W500,
+            fontSize = 14.sp,
+            color = gray100
+        ),
+        // P/14/Regular
+        labelSmall = TextStyle(
+            fontFamily = preTendFont,
+            fontWeight = FontWeight.W400,
+            fontSize = 14.sp,
+            color = gray100
+        ),
+        // P/12/Medium
+        headlineLarge = TextStyle(
+            fontFamily = preTendFont,
+            fontWeight = FontWeight.W500,
+            fontSize = 12.sp,
+            color = gray100
+        ),
+        // P/12/Regular
+        headlineMedium = TextStyle(
+            fontFamily = preTendFont,
+            fontWeight = FontWeight.W500,
+            fontSize = 12.sp,
+            color = gray100
+        ),
+        // P/10/Medium
+        headlineSmall = TextStyle(
+            fontFamily = preTendFont,
+            fontWeight = FontWeight.W500,
+            fontSize = 10.sp,
+            color = gray100
+        )
     )
-     */
-)
