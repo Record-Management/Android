29064d1704fc989f9fde9a3fdb14f2e7ac141b76
이건희 <leech9876@naver.com>
2025-09-07 14:15:52 +0900

    [ANDROID-13] style : Color 적용

--- core/designsystem/src/main/java/see/day/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/see/day/designsystem/theme/Theme.kt
@@ -8,29 +8,40 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+private val DarkColorScheme =
+    darkColorScheme(
+        primary = primaryColor,
+        onPrimary = onPrimary,
+        background = backgroundColor,
+        onBackground = onBackground,
+        onPrimaryContainer = Color.White,
+        primaryContainer = Color.White,
+        surface = surface,
+        onSurface = onSurface,
+        surfaceContainer = Color.White,
+        error = Color.White,
+        onError = onError,
+        errorContainer = Color.White
+    )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-     */
-)
+private val LightColorScheme =
+    lightColorScheme(
+        primary = primaryColor,
+        onPrimary = onPrimary,
+        background = backgroundColor,
+        onBackground = onBackground,
+        surfaceContainer = Color.White,
+        onPrimaryContainer = Color.White,
+        primaryContainer = Color.White,
+        surface = surface,
+        onSurface = onSurface,
+        error = Color.White,
+        onError = onError,
+        errorContainer = Color.White
+    )
 
 @Composable
 fun SeeDayTheme(
