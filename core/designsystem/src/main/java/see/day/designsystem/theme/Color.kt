fa9d95a73ddf6c2968a2ea679aeac54baf7bc093
이건희 <57277631+hegunhee@users.noreply.github.com>
2025-09-07 16:48:50 +0900

    [ANDROID-13 ] theme 적용 (#26)
    * [ANDROID-13] style : Color 적용
    
    * [ANDROID-13] style : Font 적용
    
    * [ANDROID-13] style : 폰트 프리뷰 생성
    
    행간이나 텍스트가 잘 떨어지는지 확인했다

--- core/designsystem/src/main/java/see/day/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/see/day/designsystem/theme/Color.kt
@@ -2,6 +2,23 @@ package see.day.designsystem.theme
 
 import androidx.compose.ui.graphics.Color
 
+val primaryColor = Color(0xFFFF6000)
+val onPrimary = Color(0xFFFFCA93)
+val onSecondary = Color(0xFFFFF0E1)
+
+val onError = Color(0xFFFF3B30)
+
+val gray100 = Color(0xFF212121)
+val gray90 = Color(0xFF424242)
+val gray80 = Color(0xFF616161)
+val gray70 = Color(0xFF757575)
+val gray60 = Color(0xFF9E9E9E)
+val gray50 = Color(0xFFBDBDBD)
+val gray40 = Color(0xFFE0E0E0)
+val gray30 = Color(0xFFEEEEEE)
+val gray20 = Color(0xFFF5F5F5)
+val gray10 = Color(0xFFFAFAFA)
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -9,3 +26,14 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val secondary = Color(0xFFFFFFFF)
+val backgroundColor = Color(0xFFFFFFFF)
+val tertiary = Color(0xFFFFFFFF)
+val surface = Color(0xFFFFFFFF)
+val onTertiary = Color(0xFFFFFFFF)
+val onBackground = Color(0xFFFFFFFF)
+val onSurface = Color(0xFFFFFFFF)
+val onPrimaryContainer = Color(0xFFFFFFFF)
+val onSecondaryContainer = Color(0xFFFFFFFF)
+val mainHomeActionBarColor = Color(0xFFFFFFFF)
--- core/designsystem/src/main/java/see/day/designsystem/theme/Type.kt
+++ core/designsystem/src/main/java/see/day/designsystem/theme/Type.kt
@@ -1,34 +1,163 @@
 package see.day.designsystem.theme
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import see.day.designsystem.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+val preTendFont =
+    FontFamily(
+        Font(R.font.pretendard_regular, FontWeight.Normal, FontStyle.Normal),
+        Font(R.font.pretendard_bold, FontWeight.Bold, FontStyle.Normal),
+        Font(R.font.pretendard_semibold, FontWeight.SemiBold, FontStyle.Normal)
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+
+val Typography =
+    Typography(
+        // P/22/Bold
+        bodyLarge =
+            TextStyle(
+                fontFamily = preTendFont,
+                fontWeight = FontWeight.W700,
+                fontSize = 22.sp,
+                lineHeight = 22.sp * 1.5f,
+                color = gray100
+            ),
+        // P/20/Bold
+        bodyMedium =
+            TextStyle(
+                fontFamily = preTendFont,
+                fontWeight = FontWeight.W700,
+                fontSize = 20.sp,
+                lineHeight = 20.sp * 1.5f,
+                color = gray100
+            ),
+        // P/20/SemiBold
+        bodySmall =
+            TextStyle(
+                fontFamily = preTendFont,
+                fontWeight = FontWeight.W600,
+                fontSize = 20.sp,
+                lineHeight = 20.sp * 1.5f,
+                color = gray100
+            ),
+        // P/18/SemiBold
+        titleLarge =
+            TextStyle(
+                fontFamily = preTendFont,
+                fontWeight = FontWeight.W600,
+                fontSize = 18.sp,
+                lineHeight = 18.sp * 1.5f,
+                color = gray100
+            ),
+        // P/18/Medium
+        titleMedium =
+            TextStyle(
+                fontFamily = preTendFont,
+                fontWeight = FontWeight.W500,
+                fontSize = 18.sp,
+                lineHeight = 18.sp * 1.5f,
+                color = gray100
+            ),
+        // P/16/SemiBold
+        titleSmall =
+            TextStyle(
+                fontFamily = preTendFont,
+                fontWeight = FontWeight.W600,
+                fontSize = 16.sp,
+                lineHeight = 16.sp * 1.5f,
+                color = gray100
+            ),
+        // P/16/Medium
+        displayLarge = TextStyle(
+            fontFamily = preTendFont,
+            fontWeight = FontWeight.W500,
+            fontSize = 16.sp,
+            lineHeight = 16.sp * 1.5f,
+            color = gray100
+        ),
+        // P/16/Regular
+        displayMedium =
+            TextStyle(
+                fontFamily = preTendFont,
+                fontWeight = FontWeight.W400,
+                fontSize = 16.sp,
+                lineHeight = 16.sp * 1.5f,
+                color = gray100
+            ),
+        // P/14/Semibold
+        displaySmall = TextStyle(
+            fontFamily = preTendFont,
+            fontWeight = FontWeight.W600,
+            fontSize = 14.sp,
+            lineHeight = 14.sp * 1.5f,
+            color = gray100
+        ),
+        // P/14/Medium
+        labelMedium = TextStyle(
+            fontFamily = preTendFont,
+            fontWeight = FontWeight.W500,
+            fontSize = 14.sp,
+            lineHeight = 14.sp * 1.5f,
+            color = gray100
+        ),
+        // P/14/Regular
+        labelSmall = TextStyle(
+            fontFamily = preTendFont,
+            fontWeight = FontWeight.W400,
+            fontSize = 14.sp,
+            lineHeight = 14.sp * 1.5f,
+            color = gray100
+        ),
+        // P/12/Medium
+        headlineLarge = TextStyle(
+            fontFamily = preTendFont,
+            fontWeight = FontWeight.W500,
+            fontSize = 12.sp,
+            lineHeight = 12.sp * 1.5f,
+            color = gray100
+        ),
+        // P/12/Regular
+        headlineMedium = TextStyle(
+            fontFamily = preTendFont,
+            fontWeight = FontWeight.W500,
+            fontSize = 12.sp,
+            lineHeight = 12.sp * 1.5f,
+            color = gray100
+        ),
+        // P/10/Medium
+        headlineSmall = TextStyle(
+            fontFamily = preTendFont,
+            fontWeight = FontWeight.W500,
+            fontSize = 10.sp,
+            lineHeight = 10.sp * 1.0f,
+            color = gray100
+        )
     )
-     */
-)
+
+@Preview
+@Composable
+fun TypoPreview() {
+    val string = "개미가 줄을 타고 올라갑니다."
+
+    Column {
+        Text(
+            text = string,
+            style = Typography.bodyLarge
+        )
+
+        Text(
+            text = string,
+            style = Typography.headlineSmall
+        )
+    }
+
+}
