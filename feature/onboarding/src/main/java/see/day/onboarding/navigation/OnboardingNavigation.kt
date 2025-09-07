d15d675c6255d74da808a7d3c63852b21b9ac6d8
이건희 <leech9876@naver.com>
2025-09-07 18:46:48 +0900

    [ANDROID-33] feat : 기본적인 온보딩 화면 뼈대 작성

--- feature/onboarding/src/main/java/see/day/onboarding/navigation/OnboardingNavigation.kt
+++ feature/onboarding/src/main/java/see/day/onboarding/navigation/OnboardingNavigation.kt
@@ -1,11 +1,10 @@
 package see.day.onboarding.navigation
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Text
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import see.day.onboarding.screen.OnboardingScreenRoot
 
 const val ONBOARDING_ROUTE = "ONBOARDING"
 
@@ -15,8 +14,6 @@ fun NavController.navigateOnboarding(navOptions: NavOptions? = null) {
 
 fun NavGraphBuilder.onboardingNavigation() {
     composable(ONBOARDING_ROUTE) {
-        Column {
-            Text("온보딩")
-        }
+        OnboardingScreenRoot()
     }
 }
---
+++ feature/onboarding/src/main/java/see/day/onboarding/screen/OnboardingScreenRoot.kt
@@ -0,0 +1,40 @@
+package see.day.onboarding.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import see.day.designsystem.theme.SeeDayTheme
+
+@Composable
+internal fun OnboardingScreenRoot() {
+    OnboardingScreen()
+}
+
+@Composable
+internal fun OnboardingScreen(
+    modifier : Modifier = Modifier,
+) {
+    Scaffold(
+        modifier = modifier.systemBarsPadding()
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            Text("온보딩")
+        }
+
+    }
+}
+
+@Preview
+@Composable
+private fun OnboardingScreenPreview() {
+    SeeDayTheme {
+        OnboardingScreen()
+    }
+}
