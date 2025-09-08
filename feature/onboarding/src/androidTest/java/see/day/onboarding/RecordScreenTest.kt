3cffdcac9a322828159555a4cf5ebab5e0c6d83f
이건희 <leech9876@naver.com>
2025-09-08 15:37:38 +0900

    [ANDROID-33] feat : 온보딩 화면 테스트 코드 작성
    상단의 설명 아이콘과 텍스트가 잘 나오는지
    클릭 이벤트가 정상 작동 하는지를 테스트했다

---
+++ feature/onboarding/src/androidTest/java/see/day/onboarding/RecordScreenTest.kt
@@ -0,0 +1,102 @@
+package see.day.onboarding
+
+import android.content.Context
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsEnabled
+import androidx.compose.ui.test.assertIsNotDisplayed
+import androidx.compose.ui.test.assertIsNotEnabled
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import see.day.model.record.RecordType
+import see.day.onboarding.screen.OnboardingScreen
+import see.day.onboarding.state.onboarding.OnboardingUiState
+
+class RecordScreenTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    private lateinit var context: Context
+
+    @Before
+    fun initContext() {
+        context = composeTestRule.activity.baseContext
+    }
+
+    @Test
+    fun givenInitOnboarding_whenScreening_shownRecordTitleAndBody() {
+        val uiState = OnboardingUiState.init
+        composeTestRule.setContent {
+            OnboardingScreen(
+                uiState = OnboardingUiState.init,
+                uiEvent = {}
+            )
+        }
+
+        composeTestRule
+            .onNodeWithText(context.getString(R.string.record_message))
+            .assertIsDisplayed()
+
+        composeTestRule
+            .onNodeWithContentDescription("온보딩 ${uiState.onboardingScreenState.ordinal} 번째 아이콘")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun given_whenClickAllTypes_shownEnabledNextButton() {
+        composeTestRule.setContent {
+            OnboardingScreen(
+                uiState = OnboardingUiState.init,
+                uiEvent = {}
+            )
+        }
+
+        RecordType.entries.forEach { type ->
+            composeTestRule
+                .onNodeWithContentDescription("$type Image")
+                .performClick()
+
+            composeTestRule
+                .onNodeWithContentDescription("checked")
+                .assertIsDisplayed()
+
+            composeTestRule
+                .onNodeWithText("다음")
+                .assertIsEnabled()
+        }
+    }
+
+    @Test
+    fun given_whenClickDouble_shownDisabledNextButton() {
+        composeTestRule.setContent {
+            OnboardingScreen(
+                uiState = OnboardingUiState.init,
+                uiEvent = {}
+            )
+        }
+
+        RecordType.entries.forEach { type ->
+            composeTestRule
+                .onNodeWithContentDescription("$type Image")
+                .performClick()
+
+            composeTestRule
+                .onNodeWithContentDescription("$type Image")
+                .performClick()
+
+            composeTestRule
+                .onNodeWithContentDescription("checked")
+                .assertIsNotDisplayed()
+
+            composeTestRule
+                .onNodeWithText("다음")
+                .assertIsNotEnabled()
+        }
+    }
+}
