383b224cc8634f475bdb56443ac6fc46d15296dd
이건희 <leech9876@naver.com>
2025-09-09 22:56:16 +0900

    [ANDROID-38] test : 알람 스크린 테스트 작성

---
+++ feature/onboarding/src/androidTest/java/see/day/onboarding/AlertScreenTest.kt
@@ -0,0 +1,47 @@
+package see.day.onboarding
+
+import android.content.Context
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import see.day.onboarding.screen.OnboardingScreen
+import see.day.onboarding.state.OnboardingScreenState.ALERT
+import see.day.onboarding.state.OnboardingScreenState.BIRTHDAY
+import see.day.onboarding.state.onboarding.OnboardingUiState
+
+class AlertScreenTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    private lateinit var context: Context
+
+    @Before
+    fun initContext() {
+        context = composeTestRule.activity.baseContext
+    }
+
+    @Test
+    fun givenInitOnboarding_whenScreening_shownRecordTitleAndBody() {
+        val uiState = OnboardingUiState.init.copy(onboardingScreenState = ALERT)
+        composeTestRule.setContent {
+            OnboardingScreen(
+                uiState = uiState,
+                uiEvent = {}
+            )
+        }
+
+        composeTestRule
+            .onNodeWithText(context.getString(R.string.alert_message))
+            .assertIsDisplayed()
+
+        composeTestRule
+            .onNodeWithContentDescription("온보딩 ${uiState.onboardingScreenState.ordinal} 번째 아이콘")
+            .assertIsDisplayed()
+    }
+}
