7d6c66137a1cb0e436305f125fc8e49d378b3cf7
이건희 <leech9876@naver.com>
2025-09-10 14:13:28 +0900

    [ANDROID-35] style : 앱 바의 아이콘 색상이 회색으로 나오던 것 수정
    앱의 색상이 흰색이므로 검정색이 좋다

--- core/designsystem/src/main/java/see/day/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/see/day/designsystem/theme/Theme.kt
@@ -1,11 +1,16 @@
 package see.day.designsystem.theme
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme =
     darkColorScheme(
@@ -52,7 +57,15 @@ fun SeeDayTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
-
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.navigationBarColor = Color.Transparent.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
+            WindowCompat.getInsetsController(window, view).isAppearanceLightNavigationBars = true
+        }
+    }
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
