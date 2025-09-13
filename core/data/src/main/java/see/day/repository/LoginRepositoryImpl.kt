4d4ff43a02f3c4795859f5260e830983f3c5a80b
이건희 <leech9876@naver.com>
2025-09-13 15:44:16 +0900

    [ANDROID-47] feat : 내부 저장된 플래그가 아닌 accessToken 요청으로 앱 초기 상태를 설정하도록 변경

--- core/data/src/main/java/see/day/repository/LoginRepositoryImpl.kt
+++ core/data/src/main/java/see/day/repository/LoginRepositoryImpl.kt
@@ -1,10 +1,15 @@
 package see.day.repository
 
+import kotlinx.coroutines.CoroutineScope
 import javax.inject.Inject
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import see.day.datastore.DataStoreDataSource
+import see.day.di.ApiModule
 import see.day.domain.repository.LoginRepository
 import see.day.mapper.toDto
 import see.day.model.exception.NoDataException
@@ -12,12 +17,15 @@ import see.day.model.login.SocialLogin
 import see.day.model.navigation.AppStartState
 import see.day.model.navigation.AppStartState.HOME
 import see.day.model.navigation.AppStartState.ONBOARDING
+import see.day.network.AuthService
 import see.day.network.LoginService
+import see.day.network.dto.auth.RefreshTokenRequest
 import see.day.utils.ErrorUtils.createResult
 
 class LoginRepositoryImpl @Inject constructor(
     private val dataSource: DataStoreDataSource,
-    private val loginService: LoginService
+    private val loginService: LoginService,
+    @ApiModule.Auth private val authService: AuthService
 ) : LoginRepository {
 
     override suspend fun login(socialLogin: SocialLogin): Result<AppStartState> {
@@ -43,7 +51,26 @@ class LoginRepositoryImpl @Inject constructor(
         }
     }
 
-    override fun getLoginState(): Flow<AppStartState> {
-        return dataSource.getAppStartState()
+    override fun getLoginState(): Flow<AppStartState> = flow {
+        if (!dataSource.hasToken().first()) {
+            AppStartState.LOGIN
+        } else {
+            val refreshToken = (RefreshTokenRequest(dataSource.getRefreshToken().first() ?: return@flow emit(AppStartState.LOGIN)))
+            runCatching { authService.refresh(refreshToken.toRequestBody()) }
+                .onSuccess {
+                    val response = it.data ?: return@flow emit(AppStartState.LOGIN)
+                    CoroutineScope(Dispatchers.IO).launch {
+                        dataSource.saveAccessToken(response.accessToken)
+                    }
+                    return@flow if (response.user.onboardingCompleted) {
+                        emit(HOME)
+                    } else {
+                        emit(ONBOARDING)
+                    }
+                }.onFailure {
+                    return@flow emit(AppStartState.LOGIN)
+                }
+        }
+
     }
 }
