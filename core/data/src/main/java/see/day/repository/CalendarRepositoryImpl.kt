9a29bbfa4494574808067deff0a487894ec96344
이건희 <leech9876@naver.com>
2025-09-17 21:13:37 +0900

    [ANDROID-53] feat : 날짜 기록 레포지토리 코드 작성

--- core/data/src/main/java/see/day/repository/CalendarRepositoryImpl.kt
+++ core/data/src/main/java/see/day/repository/CalendarRepositoryImpl.kt
@@ -1,7 +1,8 @@
 package see.day.repository
 
 import see.day.domain.repository.CalendarRepository
-import see.day.mapper.toModel
+import see.day.mapper.calendar.toModel
+import see.day.model.calendar.DailyDetailRecord
 import see.day.model.calendar.MonthlyRecord
 import see.day.model.exception.NoDataException
 import see.day.network.CalendarService
@@ -15,6 +16,10 @@ class CalendarRepositoryImpl @Inject constructor(
 
     override suspend fun getMonthlyRecords(year: Int, month: Int, types: Array<String>): Result<MonthlyRecord> {
         return createResult { calendarService.getMonthlyRecords(year, month, types).data?.toModel() ?: throw NoDataException() }
+    }
+
+    override suspend fun getDailyDetailRecords(date: String): Result<DailyDetailRecord> {
+        return createResult { calendarService.getDailyRecordData(date).data?.toModel() ?: throw NoDataException() }
     }
 
 }
