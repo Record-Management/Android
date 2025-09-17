4c50f3e496398bed403e2e834c8c337a1ea88035
이건희 <leech9876@naver.com>
2025-09-17 19:47:47 +0900

    [ANDROID-53] feat : 유저 정보를 불러오는 UserRepository작성
    테스트코드도 작성하였다.

--- core/domain/src/main/java/see/day/domain/repository/UserRepository.kt
+++ core/domain/src/main/java/see/day/domain/repository/UserRepository.kt
@@ -1,7 +1,9 @@
 package see.day.domain.repository
 
+import see.day.model.record.RecordType
 import see.day.model.user.OnboardingComplete
 
 interface UserRepository {
     suspend fun onboardComplete(onboardingComplete: OnboardingComplete) : Result<Unit>
+    suspend fun getMainRecordType() : Result<RecordType>
 }
