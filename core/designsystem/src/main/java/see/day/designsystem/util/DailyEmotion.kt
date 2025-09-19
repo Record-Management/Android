75d9217a2be2f149a54745ee7633cb7ce44cf524
이건희 <leech9876@naver.com>
2025-09-19 23:12:41 +0900

    [ANDROID-54] feat : 이모티콘 enum 클래스 작성, UI 작성

---
+++ core/designsystem/src/main/java/see/day/designsystem/util/DailyEmotion.kt
@@ -0,0 +1,15 @@
+package see.day.designsystem.util
+
+import see.day.designsystem.R
+
+enum class DailyEmotion(val largeIconRes: Int) {
+    SHY(R.drawable.emotion_shy_large),
+    WINK(R.drawable.emotion_wink_large),
+    NORMAL(R.drawable.emotion_normal_large),
+    SUNGLASSES(R.drawable.emotion_sunglasses_large),
+    LOVE(R.drawable.emotion_love_large),
+    FRUSTRATION(R.drawable.emotion_frustration_large),
+    HUM(R.drawable.emotion_hum_large),
+    SAD(R.drawable.emotion_sad_large),
+    ANGRY(R.drawable.emotion_angry_large),
+}
--- feature/daily/src/main/java/see/day/daily/screen/DailyScreenRoot.kt
+++ feature/daily/src/main/java/see/day/daily/screen/DailyScreenRoot.kt
@@ -1,29 +1,120 @@
 package see.day.daily.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.key
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import see.day.daily.R
 import see.day.designsystem.theme.SeeDayTheme
+import see.day.designsystem.theme.gray100
+import see.day.designsystem.util.DailyEmotion
 
 @Composable
 internal fun DailyScreenRoot(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    onClickBackButton: () -> Unit
 ) {
-    DailyScreen(modifier)
+    DailyScreen(modifier, onClickBackButton)
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun DailyScreen(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    onClickBackButton : () -> Unit,
 ) {
-    Text("Daily Screen")
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Row(
+                        modifier = modifier.fillMaxWidth()
+                    ) {
+                        Spacer(modifier = modifier.weight(1f))
+                        Image(
+                            modifier = modifier.padding(top = 16.dp, end = 16.dp).clickable { onClickBackButton() },
+                            painter = painterResource(R.drawable.ic_close),
+                            contentDescription = "뒤로가기 버튼"
+                        )
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = modifier.padding(innerPadding).fillMaxHeight().fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                modifier = modifier.padding(top = 60.dp),
+                text = "오늘의 감정을 선택해주세요",
+                style = MaterialTheme.typography.bodySmall
+            )
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(3),
+                verticalArrangement = Arrangement.spacedBy(24.dp),
+                horizontalArrangement = Arrangement.spacedBy(24.dp),
+                modifier = modifier.padding(top = 40.dp).padding(horizontal = 43.dp).fillMaxWidth()
+            ) {
+                items(items = DailyEmotion.entries.toList(), key = {it}) { emotion ->
+                    Image(
+                        painter = painterResource(emotion.largeIconRes),
+                        contentDescription = "emotion ${emotion.name}",
+                        modifier = modifier.size(80.dp),
+                        contentScale = ContentScale.FillBounds
+                    )
+                }
+            }
+            Spacer(modifier = modifier.weight(1f))
+            Text(
+                text = "기록 방식을 바꿀래요",
+                style = MaterialTheme.typography.labelMedium.copy(
+                    textDecoration = TextDecoration.Underline
+                ),
+                modifier = modifier.padding(bottom = 52.dp).systemBarsPadding()
+            )
+        }
+    }
 }
 
 @Preview
 @Composable
 private fun DailyScreenPreview() {
     SeeDayTheme {
-        DailyScreen()
+        DailyScreen(
+            onClickBackButton = {}
+        )
     }
 }
