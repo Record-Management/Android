1f4d968d395fc402462efce5c70cf8a7132d98af
이건희 <leech9876@naver.com>
2025-09-20 23:13:04 +0900

    [ANDROID-54] feat : DailyEmotion과 아이콘 정보를 가져오는 변수 분리
    Serializable을 위해서라면 분리해야 한다.

--- core/designsystem/src/main/java/see/day/designsystem/util/DailyEmotion.kt
+++ core/designsystem/src/main/java/see/day/designsystem/util/DailyEmotion.kt
@@ -1,15 +1,31 @@
 package see.day.designsystem.util
 
+import kotlinx.serialization.Serializable
 import see.day.designsystem.R
 
-enum class DailyEmotion(val largeIconRes: Int) {
-    SHY(R.drawable.emotion_shy_large),
-    WINK(R.drawable.emotion_wink_large),
-    NORMAL(R.drawable.emotion_normal_large),
-    SUNGLASSES(R.drawable.emotion_sunglasses_large),
-    LOVE(R.drawable.emotion_love_large),
-    FRUSTRATION(R.drawable.emotion_frustration_large),
-    HUM(R.drawable.emotion_hum_large),
-    SAD(R.drawable.emotion_sad_large),
-    ANGRY(R.drawable.emotion_angry_large),
+@Serializable
+enum class DailyEmotion {
+    SHY,
+    WINK,
+    NORMAL,
+    SUNGLASSES,
+    LOVE,
+    FRUSTRATION,
+    HUM,
+    SAD,
+    ANGRY,
 }
+
+// DrawbleRes는 런타임에 결정되므로 직렬화가 불가능
+val DailyEmotion.largeIconRes: Int
+    get() = when(this) {
+        DailyEmotion.SHY -> R.drawable.emotion_shy_large
+        DailyEmotion.WINK -> R.drawable.emotion_wink_large
+        DailyEmotion.NORMAL -> R.drawable.emotion_normal_large
+        DailyEmotion.SUNGLASSES -> R.drawable.emotion_sunglasses_large
+        DailyEmotion.LOVE -> R.drawable.emotion_love_large
+        DailyEmotion.FRUSTRATION -> R.drawable.emotion_frustration_large
+        DailyEmotion.HUM -> R.drawable.emotion_hum_large
+        DailyEmotion.SAD -> R.drawable.emotion_sad_large
+        DailyEmotion.ANGRY -> R.drawable.emotion_angry_large
+    }
