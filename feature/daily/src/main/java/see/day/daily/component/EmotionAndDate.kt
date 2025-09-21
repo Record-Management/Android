0eb587f24e504f99653cf7e14375a6eab045128e
이건희 <leech9876@naver.com>
2025-09-21 23:25:16 +0900

    [ANDROID-57] feat : 감정과 날짜를 표시하는 컴포저블 분리

---
+++ feature/daily/src/main/java/see/day/daily/component/EmotionAndDate.kt
@@ -0,0 +1,75 @@
+package see.day.daily.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import see.day.designsystem.theme.SeeDayTheme
+import see.day.designsystem.util.DailyEmotion
+import see.day.designsystem.util.largeIconRes
+import see.day.model.time.DateTime
+import see.day.model.time.formatter.KoreanDateTimeFormatter
+
+@Composable
+internal fun EmotionAndDate(
+    modifier: Modifier = Modifier,
+    emotion: DailyEmotion,
+    currentDate: String,
+    currentTime: String,
+    onClickEmotion : () -> Unit
+) {
+    Row(
+        modifier = modifier.padding(top = 10.dp),
+        verticalAlignment = Alignment.Bottom
+    ) {
+        Image(
+            painter = painterResource(emotion.largeIconRes),
+            contentDescription = "emotion ${emotion.name}",
+            modifier = modifier
+                .size(80.dp)
+                .clickable { onClickEmotion() },
+            contentScale = ContentScale.FillBounds
+        )
+        Column(
+            modifier = modifier
+                .padding(vertical = (14.5).dp)
+                .padding(start = 16.dp),
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            Text(
+                text = currentDate,
+                style = MaterialTheme.typography.titleSmall,
+            )
+            Text(
+                text = currentTime,
+                style = MaterialTheme.typography.displayLarge,
+                modifier = modifier.padding(top = 3.dp)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun EmotionAndDatePreview() {
+    SeeDayTheme {
+        EmotionAndDate(
+            emotion = DailyEmotion.FRUSTRATION,
+            currentDate = KoreanDateTimeFormatter(DateTime.now(DateTime.korea)).formatDate(),
+            currentTime = KoreanDateTimeFormatter(DateTime.now(DateTime.korea)).formatTime(),
+            onClickEmotion = {}
+        )
+    }
+}
--- feature/daily/src/main/java/see/day/daily/screen/DailyDetailScreenRoot.kt
+++ feature/daily/src/main/java/see/day/daily/screen/DailyDetailScreenRoot.kt
@@ -1,62 +1,115 @@
 package see.day.daily.screen
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import see.day.daily.R
+import see.day.daily.component.EmotionAndDate
 import see.day.daily.util.DailyDetailType
+import see.day.designsystem.theme.SeeDayTheme
+import see.day.designsystem.util.DailyEmotion
+import see.day.model.record.RecordType
+import see.day.model.time.DateTime
+import see.day.model.time.formatter.KoreanDateTimeFormatter
+import see.day.ui.dialog.RecordDetailBackDialog
+import see.day.ui.topbar.DetailRecordTopBar
 
 @Composable
 internal fun DailyDetailScreenRoot(modifier: Modifier = Modifier, dailyDetailType: DailyDetailType, onClickPopHome: () -> Unit) {
+    var openBackDialog by remember { mutableStateOf(false) }
+    val currentTime by remember { mutableStateOf(DateTime.now(DateTime.korea)) }
+    val timeFormatter by remember { mutableStateOf(KoreanDateTimeFormatter(currentTime)) }
+    if (openBackDialog) {
+        RecordDetailBackDialog(
+            modifier = modifier,
+            onDismiss = { openBackDialog = false },
+            onBackRecordDetail = onClickPopHome,
+            title = R.string.record_close_dialog_title,
+            body = R.string.record_close_dialog_body,
+        )
+    }
     BackHandler {
-        onClickPopHome()
+        openBackDialog = true
+    }
+    val emotion = when (dailyDetailType) {
+        is DailyDetailType.WriteDailyDetail -> {
+            dailyDetailType.emotion
+        }
+
+        is DailyDetailType.EditDailyDetail -> {
+            DailyEmotion.LOVE
+        }
     }
     DailyDetailScreen(
         modifier = modifier,
         dailyDetailType = dailyDetailType,
-        onClickBackButton = onClickPopHome
+        onClickBackButton = { openBackDialog = true },
+        emotion = emotion,
+        currentDate = timeFormatter.formatDate(),
+        currentTime = timeFormatter.formatTime()
     )
 }
 
 @Composable
-internal fun DailyDetailScreen(modifier: Modifier = Modifier, dailyDetailType: DailyDetailType, onClickBackButton: () -> Unit) {
+internal fun DailyDetailScreen(
+    modifier: Modifier = Modifier,
+    dailyDetailType: DailyDetailType,
+    onClickBackButton: () -> Unit,
+    emotion: DailyEmotion,
+    currentDate: String,
+    currentTime: String
+) {
     Scaffold(
         modifier = modifier.systemBarsPadding(),
         topBar = {
-            Row {
-                Spacer(modifier = modifier.weight(1f))
-                Image(
-                    modifier = modifier
-                        .padding(top = 16.dp, end = 16.dp)
-                        .clickable { onClickBackButton() },
-                    painter = painterResource(R.drawable.ic_close),
-                    contentDescription = "뒤로가기 버튼"
-                )
+            DetailRecordTopBar(
+                modifier = modifier,
+                recordType = RecordType.DAILY,
+                onClickCloseButton = onClickBackButton
+            )
+
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = modifier
+                .padding(innerPadding)
+                .padding(start = 16.dp)
+        ) {
+            EmotionAndDate(modifier, emotion, currentDate, currentTime,{})
+            when (dailyDetailType) {
+                is DailyDetailType.WriteDailyDetail -> {
+                    Text("쓰기 ${dailyDetailType.emotion}")
+                }
+
+                is DailyDetailType.EditDailyDetail -> {
+                    Text("수정하기 ${dailyDetailType.id}")
+                }
             }
         }
-    ) {
-
     }
-    Column {
-        when (dailyDetailType) {
-            is DailyDetailType.WriteDailyDetail -> {
-                Text("쓰기 ${dailyDetailType.emotion}")
-            }
+}
 
-            is DailyDetailType.EditDailyDetail -> {
-                Text("수정하기 ${dailyDetailType.id}")
-            }
-        }
+@Preview
+@Composable
+private fun DailyDetailWriteScreen() {
+    SeeDayTheme {
+        DailyDetailScreen(
+            dailyDetailType = DailyDetailType.WriteDailyDetail(DailyEmotion.FRUSTRATION),
+            onClickBackButton = {},
+            emotion = DailyEmotion.FRUSTRATION,
+            currentDate = KoreanDateTimeFormatter(DateTime.now(DateTime.korea)).formatDate(),
+            currentTime = KoreanDateTimeFormatter(DateTime.now(DateTime.korea)).formatTime()
+        )
     }
 }
