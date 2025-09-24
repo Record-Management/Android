bb26914c725b839ce10830ab93a15655e1158470
이건희 <leech9876@naver.com>
2025-09-24 17:46:02 +0900

    [ANDROID-57] feat : 작성하기 버튼 작성 및 상세 화면 작성

--- feature/daily/src/main/java/see/day/daily/screen/DailyDetailScreenRoot.kt
+++ feature/daily/src/main/java/see/day/daily/screen/DailyDetailScreenRoot.kt
@@ -1,6 +1,6 @@
 package see.day.daily.screen
 
-import android.widget.Space
+import android.net.Uri
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -19,6 +20,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,11 +29,14 @@ import see.day.daily.component.EmotionAndDate
 import see.day.daily.component.EmotionSelectBottomSheet
 import see.day.daily.util.DailyDetailType
 import see.day.designsystem.theme.SeeDayTheme
+import see.day.designsystem.theme.gray60
 import see.day.designsystem.util.DailyEmotion
 import see.day.model.record.RecordType
 import see.day.model.time.DateTime
 import see.day.model.time.formatter.KoreanDateTimeFormatter
+import see.day.ui.button.CompleteButtonNoHorizontalPadding
 import see.day.ui.dialog.RecordDetailBackDialog
+import see.day.ui.photo.RecordDetailPhotoRow
 import see.day.ui.textField.RecordWriteTextField
 import see.day.ui.topbar.DetailRecordTopBar
 
@@ -81,16 +86,32 @@ internal fun DailyDetailScreenRoot(modifier: Modifier = Modifier, dailyDetailTyp
         )
     }
     val (text, onChangedText) = rememberSaveable { mutableStateOf("") }
+    var uris by remember { mutableStateOf(listOf<Uri>()) }
+    val onClickAddPhotos: (List<Uri>) -> Unit = { selectedUris ->
+        val addedUris = (uris + selectedUris).distinct()
+        uris = if (addedUris.size <= 3) {
+            addedUris
+        } else {
+            addedUris.subList(0, 3)
+        }
+    }
     DailyDetailScreen(
         modifier = modifier,
         dailyDetailType = dailyDetailType,
         emotion = emotion,
         text = text,
+        uris = uris,
         currentDate = timeFormatter.formatDate(),
         currentTime = timeFormatter.formatTime(),
         onClickBackButton = { openBackDialog = true },
         onClickEmotion = { openSelectEmotionDialog = true },
-        onChangedText
+        onTextChanged = onChangedText,
+        onClickAddPhotos = onClickAddPhotos,
+        onRemovePhotos = { removedPhoto ->
+            uris = uris.filter {
+                removedPhoto != it
+            }
+        }
     )
 }
 
@@ -102,19 +123,24 @@ internal fun DailyDetailScreen(
     text: String,
     currentDate: String,
     currentTime: String,
+    uris: List<Uri>,
     onClickBackButton: () -> Unit,
     onClickEmotion: () -> Unit,
-    onTextChanged: (String) -> Unit
+    onTextChanged: (String) -> Unit,
+    onClickAddPhotos: (List<Uri>) -> Unit,
+    onRemovePhotos: (Uri) -> Unit
 ) {
+    val context = LocalContext.current
     Scaffold(
-        modifier = modifier.systemBarsPadding().background(Color.White),
+        modifier = modifier
+            .systemBarsPadding()
+            .background(Color.White),
         topBar = {
             DetailRecordTopBar(
                 modifier = modifier,
                 recordType = RecordType.DAILY,
                 onClickCloseButton = onClickBackButton
             )
-
         }
     ) { innerPadding ->
         Column(
@@ -130,15 +156,19 @@ internal fun DailyDetailScreen(
                 placeHolder = R.string.record_daily_place_holder,
                 onChangedText = onTextChanged
             )
-            when (dailyDetailType) {
-                is DailyDetailType.WriteDailyDetail -> {
-                    Text("쓰기 ${dailyDetailType.emotion}")
-                }
-
-                is DailyDetailType.EditDailyDetail -> {
-                    Text("수정하기 ${dailyDetailType.id}")
-                }
-            }
+            RecordDetailPhotoRow(modifier, context, uris, onRemovePhotos, onClickAddPhotos)
+            Text(
+                modifier = modifier.padding(top = 10.dp),
+                text = stringResource(R.string.photo_limit_text),
+                color = gray60,
+                style = MaterialTheme.typography.labelSmall
+            )
+            Spacer(modifier = modifier.weight(1f))
+            CompleteButtonNoHorizontalPadding(
+                text = stringResource(see.day.ui.R.string.write_record_text),
+                isEnabled = text.isNotEmpty(),
+                onClick = {}
+            )
         }
     }
 }
@@ -154,9 +184,12 @@ private fun DailyDetailWriteScreen() {
             text = text,
             currentDate = KoreanDateTimeFormatter(DateTime.now(DateTime.korea)).formatDate(),
             currentTime = KoreanDateTimeFormatter(DateTime.now(DateTime.korea)).formatTime(),
+            uris = listOf(),
             onClickBackButton = { },
             onClickEmotion = { },
-            onTextChanged = onTextChanged
+            onTextChanged = onTextChanged,
+            onClickAddPhotos = {},
+            onRemovePhotos = {}
         )
     }
 }
