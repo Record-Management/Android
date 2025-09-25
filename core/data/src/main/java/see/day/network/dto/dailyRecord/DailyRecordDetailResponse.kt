b2995b85c2cf2dccc9e0f182d5f10b1b275ff6f0
이건희 <leech9876@naver.com>
2025-09-25 10:36:00 +0900

    [ANDROID-60] feat : 하루 기록 저장 엔드포인트 연결코드 작성, 요청 응답 객체 생성

---
+++ core/data/src/main/java/see/day/network/dto/dailyRecord/DailyRecordDetailResponse.kt
@@ -0,0 +1,21 @@
+package see.day.network.dto.dailyRecord
+
+import kotlinx.serialization.Serializable
+import see.day.network.decoder.FlexibleDateTimeArraySerializer
+
+@Serializable
+data class DailyRecordDetailResponse(
+    val id: String,
+    val type: String,
+    val emotion: String,
+    val content: String,
+    val imageUrls: String,
+    @Serializable(with = FlexibleDateTimeArraySerializer::class)
+    val recordDate: String,
+    @Serializable(with = FlexibleDateTimeArraySerializer::class)
+    val recordTime: String,
+    @Serializable(with = FlexibleDateTimeArraySerializer::class)
+    val createdAt: String,
+    @Serializable(with = FlexibleDateTimeArraySerializer::class)
+    val updatedAt: String
+)
