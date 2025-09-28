c4295f0295bac07538e9383b59dfbf4297ce6f7a
이건희 <leech9876@naver.com>
2025-09-28 16:03:16 +0900

    [ANDROID-48] feat : 알람 설정 화면 수정

--- feature/onboarding/src/main/java/see/day/onboarding/screen/onboarding/AlertScreen.kt
+++ feature/onboarding/src/main/java/see/day/onboarding/screen/onboarding/AlertScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -44,7 +45,7 @@ internal fun AlertScreen(modifier: Modifier = Modifier, onClickComplete: (Onboar
         Image(
             painter = painterResource(R.drawable.alert_image),
             contentDescription = "알람 요청 이미지",
-            modifier = modifier.fillMaxWidth().height(335.dp),
+            modifier = modifier.fillMaxWidth().offset(x = 16.dp),
             contentScale = ContentScale.FillWidth
         )
         Spacer(modifier = modifier.weight(1f))
