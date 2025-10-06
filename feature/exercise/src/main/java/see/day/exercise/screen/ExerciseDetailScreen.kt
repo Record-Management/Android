9ea848ce4ddff3b7ef4e9acb2a62341ada4d3b27
이건희 <leech9876@naver.com>
2025-10-07 00:52:21 +0900

    [ANDROID-75] feat : 운동 재선택 바텀시트 작성

--- feature/exercise/src/main/java/see/day/exercise/screen/ExerciseDetailScreen.kt
+++ feature/exercise/src/main/java/see/day/exercise/screen/ExerciseDetailScreen.kt
@@ -5,28 +5,48 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import see.day.designsystem.theme.SeeDayTheme
+import see.day.exercise.component.ExerciseSelectBottomSheet
 import see.day.exercise.component.ExerciseTitle
 import see.day.exercise.util.ExerciseRecordPostType
 import see.day.model.record.RecordType
 import see.day.model.record.exercise.ExerciseType
 import see.day.ui.topbar.DetailRecordTopBar
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ExerciseDetailScreenRoot(editType: ExerciseRecordPostType) {
-    ExerciseDetailScreen(editType = editType)
+    var uiState by remember(editType) { mutableStateOf(editType) }
+
+    var openSelectEmotionDialog by remember { mutableStateOf(false) }
+    val sheetState = rememberModalBottomSheetState()
+    if(openSelectEmotionDialog) {
+        ExerciseSelectBottomSheet(
+            modifier = Modifier,
+            sheetState = sheetState,
+            onDismiss = {openSelectEmotionDialog = false},
+            onClickChangeExerciseType = { newExerciseType ->
+                uiState = ExerciseRecordPostType.Write(newExerciseType)
+            }
+        )
+    }
+    ExerciseDetailScreen(editType = uiState, onClickExerciseImage = { openSelectEmotionDialog = true })
 }
 
 @Composable
-internal fun ExerciseDetailScreen(modifier: Modifier = Modifier, editType: ExerciseRecordPostType) {
+internal fun ExerciseDetailScreen(modifier: Modifier = Modifier, editType: ExerciseRecordPostType, onClickExerciseImage : () -> Unit) {
     Scaffold(
         modifier = modifier.systemBarsPadding(),
         topBar = {
@@ -43,7 +63,7 @@ internal fun ExerciseDetailScreen(modifier: Modifier = Modifier, editType: Exerc
                 ExerciseTitle(
                     modifier = modifier.padding(top = 10.dp),
                     exerciseType = editType.exerciseType,
-                    onClickExerciseImage = {}
+                    onClickExerciseImage = onClickExerciseImage
                 )
             }
         }
@@ -56,6 +76,6 @@ internal fun ExerciseDetailScreen(modifier: Modifier = Modifier, editType: Exerc
 @Composable
 private fun ExerciseDetailScreenPreview() {
     SeeDayTheme {
-        ExerciseDetailScreen(editType = ExerciseRecordPostType.Write(ExerciseType.GOLF))
+        ExerciseDetailScreen(editType = ExerciseRecordPostType.Write(ExerciseType.GOLF), onClickExerciseImage = {})
     }
 }
