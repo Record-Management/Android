495ad099c2a0cf942beff8577e209d90bd16ca2e
이건희 <leech9876@naver.com>
2025-10-06 22:55:33 +0900

    [ANDROID-75] feat : 글 작성과 글 수정 타입을 분리
    하나의 UI를 사용하고 디테일만 변경할 예정이다.

--- feature/exercise/src/main/java/see/day/exercise/screen/ExerciseDetailScreen.kt
+++ feature/exercise/src/main/java/see/day/exercise/screen/ExerciseDetailScreen.kt
@@ -2,25 +2,28 @@ package see.day.exercise.screen
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import see.day.designsystem.theme.SeeDayTheme
+import see.day.exercise.util.ExerciseRecordPostType
 import see.day.model.record.exercise.ExerciseType
 
 @Composable
-fun ExerciseDetailScreenRoot(exerciseType: ExerciseType) {
-    ExerciseDetailScreen(exerciseType = exerciseType)
+fun ExerciseDetailScreenRoot(editType : ExerciseRecordPostType) {
+    ExerciseDetailScreen(editType = editType)
 }
 
 @Composable
-internal fun ExerciseDetailScreen(modifier: Modifier = Modifier, exerciseType: ExerciseType) {
-    Text(exerciseType.toString())
+internal fun ExerciseDetailScreen(modifier: Modifier = Modifier, editType: ExerciseRecordPostType) {
+    Text(editType.toString())
 }
 
 @Preview
 @Composable
 private fun ExerciseDetailScreenPreview() {
     SeeDayTheme {
-        ExerciseDetailScreen(exerciseType = ExerciseType.GOLF)
+        ExerciseDetailScreen(editType = ExerciseRecordPostType.Write(ExerciseType.GOLF))
     }
 }
