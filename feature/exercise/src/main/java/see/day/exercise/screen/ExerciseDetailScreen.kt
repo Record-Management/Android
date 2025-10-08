55b90edb99848b33c0bffa14c678ef0dce7a59d6
이건희 <leech9876@naver.com>
2025-10-08 11:43:24 +0900

    [ANDROID-77] feat : UiState와 UiEvent Screen에 적용

--- feature/exercise/src/main/java/see/day/exercise/screen/ExerciseDetailScreen.kt
+++ feature/exercise/src/main/java/see/day/exercise/screen/ExerciseDetailScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -22,12 +23,17 @@ import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import see.day.designsystem.theme.SeeDayTheme
 import see.day.designsystem.theme.gray60
 import see.day.exercise.R
 import see.day.exercise.component.ExerciseSelectBottomSheet
 import see.day.exercise.component.ExerciseTitle
+import see.day.exercise.state.ExerciseDetailUiEvent
+import see.day.exercise.state.ExerciseDetailUiState
 import see.day.exercise.util.ExerciseRecordPostType
+import see.day.exercise.viewModel.ExerciseDetailViewModel
 import see.day.model.record.RecordType
 import see.day.model.record.exercise.ExerciseType
 import see.day.ui.button.CompleteButton
@@ -39,8 +45,12 @@ import see.day.ui.topbar.DetailRecordTopBar
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ExerciseDetailScreenRoot(editType: ExerciseRecordPostType) {
-    var uiState by remember(editType) { mutableStateOf(editType) }
+fun ExerciseDetailScreenRoot(editType: ExerciseRecordPostType, viewModel: ExerciseDetailViewModel = hiltViewModel()) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    LaunchedEffect(editType) {
+        viewModel.fetchData(editType)
+    }
 
     var openSelectEmotionDialog by remember { mutableStateOf(false) }
     val sheetState = rememberModalBottomSheetState()
@@ -50,20 +60,20 @@ fun ExerciseDetailScreenRoot(editType: ExerciseRecordPostType) {
             sheetState = sheetState,
             onDismiss = { openSelectEmotionDialog = false },
             onClickChangeExerciseType = { newExerciseType ->
-                uiState = ExerciseRecordPostType.Write(newExerciseType)
+                viewModel.onEvent(ExerciseDetailUiEvent.OnExerciseTypeChanged(newExerciseType))
             }
         )
     }
-    ExerciseDetailScreen(editType = uiState, onClickExerciseImage = { openSelectEmotionDialog = true })
+    ExerciseDetailScreen(uiState = uiState, uiEvent = viewModel::onEvent, onClickExerciseImage = { openSelectEmotionDialog = true })
 }
 
 @Composable
-internal fun ExerciseDetailScreen(modifier: Modifier = Modifier, editType: ExerciseRecordPostType, onClickExerciseImage: () -> Unit) {
-    val (kcal, onKcalChanged) = remember { mutableStateOf("") }
-    val (exerciseTime, onExerciseTimeChanged) = remember { mutableStateOf("") }
-    val (stepCount, onStepCountChanged) = remember { mutableStateOf("") }
-    val (weight, onWeightChanged) = remember { mutableStateOf("") }
-    val (dailyNote, onDailyNoteChanged) = remember { mutableStateOf("") }
+internal fun ExerciseDetailScreen(
+    modifier: Modifier = Modifier,
+    uiState: ExerciseDetailUiState,
+    uiEvent: (ExerciseDetailUiEvent) -> Unit,
+    onClickExerciseImage: () -> Unit
+) {
     val focusManager = LocalFocusManager.current
     val context = LocalContext.current
 
@@ -80,76 +90,89 @@ internal fun ExerciseDetailScreen(modifier: Modifier = Modifier, editType: Exerc
                 .padding(horizontal = 16.dp)
                 .verticalScroll(rememberScrollState())
         ) {
-            if (editType is ExerciseRecordPostType.Write) {
-                ExerciseTitle(
-                    modifier = modifier.padding(top = 10.dp),
-                    exerciseType = editType.exerciseType,
-                    onClickExerciseImage = onClickExerciseImage
-                )
-                HealthStatInputField(
-                    modifier = modifier.padding(top = 24.dp),
-                    healthStat = HealthStat.Kcal,
-                    text = kcal,
-                    onTextChanged = onKcalChanged,
-                    focusManager = focusManager
-                )
-                HealthStatInputField(
-                    modifier = modifier.padding(top = 24.dp),
-                    healthStat = HealthStat.Time,
-                    text = exerciseTime,
-                    onTextChanged = onExerciseTimeChanged,
-                    focusManager = focusManager
-                )
-                HealthStatInputField(
-                    modifier = modifier.padding(top = 24.dp),
-                    healthStat = HealthStat.StepCount,
-                    text = stepCount,
-                    onTextChanged = onStepCountChanged,
-                    focusManager = focusManager
-                )
-                HealthStatInputField(
-                    modifier = modifier.padding(top = 24.dp),
-                    healthStat = HealthStat.Weight,
-                    text = weight,
-                    onTextChanged = onWeightChanged,
-                    focusManager = focusManager
-                )
-                Text(
-                    modifier = modifier.padding(top = 24.dp, bottom = 10.dp),
-                    text = "나의 하루",
-                    style = MaterialTheme.typography.displaySmall
-                )
-                RecordWriteTextField(
-                    modifier = modifier,
-                    text = dailyNote,
-                    placeHolder = see.day.ui.R.string.daily_place_holder,
-                    onChangedText = onDailyNoteChanged
-                )
-                RecordDetailPhotoRow(
-                    modifier = modifier,
-                    context = context,
-                    uris = listOf(),
-                    onRemovePhotos = { photo ->
-                    },
-                    onClickAddPhotos = { photos ->
-                    }
-                )
-                Text(
-                    modifier = modifier.padding(top = 10.dp),
-                    text = stringResource(R.string.max_photo_description),
-                    color = gray60,
-                    style = MaterialTheme.typography.labelSmall
-                )
-                CompleteButton(
-                    modifier = modifier.padding(top = 80.dp).systemBarsPadding(),
-                    text = stringResource(
-                        see.day.ui.R.string.write_record_text
-                    ),
-                    isEnabled = true,
-                    onClick = {
-                    }
-                )
-            }
+            ExerciseTitle(
+                modifier = modifier.padding(top = 10.dp),
+                exerciseType = uiState.exerciseType,
+                onClickExerciseImage = onClickExerciseImage
+            )
+            HealthStatInputField(
+                modifier = modifier.padding(top = 24.dp),
+                healthStat = HealthStat.Kcal,
+                text = uiState.caloriesBurned,
+                onTextChanged = { newCaloriesBurned ->
+                    uiEvent(ExerciseDetailUiEvent.OnCaloriesChanged(newCaloriesBurned))
+                },
+                focusManager = focusManager
+            )
+            HealthStatInputField(
+                modifier = modifier.padding(top = 24.dp),
+                healthStat = HealthStat.Time,
+                text = uiState.exerciseTimeMinutes,
+                onTextChanged = { newExerciseTime ->
+                    uiEvent(ExerciseDetailUiEvent.OnExerciseTimeChanged(newExerciseTime))
+                },
+                focusManager = focusManager
+            )
+            HealthStatInputField(
+                modifier = modifier.padding(top = 24.dp),
+                healthStat = HealthStat.StepCount,
+                text = uiState.stepCount,
+                onTextChanged = { newStepCount ->
+                    uiEvent(ExerciseDetailUiEvent.OnStepCountChanged(newStepCount))
+                },
+                focusManager = focusManager
+            )
+            HealthStatInputField(
+                modifier = modifier.padding(top = 24.dp),
+                healthStat = HealthStat.Weight,
+                text = uiState.weight,
+                onTextChanged = { newWeight ->
+                    uiEvent(ExerciseDetailUiEvent.OnWeightChanged(newWeight))
+                },
+                focusManager = focusManager
+            )
+            Text(
+                modifier = modifier.padding(top = 24.dp, bottom = 10.dp),
+                text = "나의 하루",
+                style = MaterialTheme.typography.displaySmall
+            )
+            RecordWriteTextField(
+                modifier = modifier,
+                text = uiState.dailyNote,
+                placeHolder = see.day.ui.R.string.daily_place_holder,
+                onChangedText = { newDailyNote ->
+                    uiEvent(ExerciseDetailUiEvent.OnDailyNoteChanged(newDailyNote))
+                }
+            )
+            RecordDetailPhotoRow(
+                modifier = modifier,
+                context = context,
+                uris = uiState.imageUrls,
+                onRemovePhotos = { photo ->
+                    uiEvent(ExerciseDetailUiEvent.OnRemovePhoto(photo))
+                },
+                onClickAddPhotos = { photos ->
+                    uiEvent(ExerciseDetailUiEvent.OnAddPhotos(photos))
+                }
+            )
+            Text(
+                modifier = modifier.padding(top = 10.dp),
+                text = stringResource(R.string.max_photo_description),
+                color = gray60,
+                style = MaterialTheme.typography.labelSmall
+            )
+            CompleteButton(
+                modifier = modifier
+                    .padding(top = 80.dp)
+                    .systemBarsPadding(),
+                text = stringResource(
+                    see.day.ui.R.string.write_record_text
+                ),
+                isEnabled = uiState.canSubmit,
+                onClick = {
+                    uiEvent(ExerciseDetailUiEvent.OnSaveRecord)
+                }
+            )
         }
 
     }
@@ -160,6 +183,6 @@ internal fun ExerciseDetailScreen(modifier: Modifier = Modifier, editType: Exerc
 @Composable
 private fun ExerciseDetailScreenPreview() {
     SeeDayTheme {
-        ExerciseDetailScreen(editType = ExerciseRecordPostType.Write(ExerciseType.GOLF), onClickExerciseImage = {})
+        ExerciseDetailScreen(uiState = ExerciseDetailUiState.init, uiEvent = {}, onClickExerciseImage = {})
     }
 }
