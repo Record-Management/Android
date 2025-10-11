62d72b899b3904ed74448ca18c6a0c62cfdfa89f
이건희 <leech9876@naver.com>
2025-10-11 21:49:06 +0900

    [ANDROID-81] feat : 일일 기록 삭제 API 연동

--- core/data/src/main/java/see/day/network/DailyRecordService.kt
+++ core/data/src/main/java/see/day/network/DailyRecordService.kt
@@ -2,6 +2,7 @@ package see.day.network
 
 import okhttp3.RequestBody
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
@@ -18,4 +19,9 @@ interface DailyRecordService {
         @Path("recordId") recordId: String,
         @Body requestBody: RequestBody
     ) : CommonResponse<DailyRecordResponse>
+
+    @DELETE("api/daily-records/{recordId}")
+    suspend fun deleteDailyRecord(
+        @Path("recordId") recordId: String
+    ) : CommonResponse<Unit>
 }
