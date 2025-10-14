83d17c7778418575a0af254ef65070b6aae9ced2
이건희 <leech9876@naver.com>
2025-10-15 00:01:13 +0900

    [ANDROID-86] feat : 운동 기록 수정 DTO, Model 작성

--- core/data/src/main/java/see/day/mapper/record/ExerciseMapper.kt
+++ core/data/src/main/java/see/day/mapper/record/ExerciseMapper.kt
@@ -1,18 +1,33 @@
 package see.day.mapper.record
 
+import see.day.model.record.exercise.ExerciseRecordEdit
 import see.day.model.record.exercise.ExerciseRecordInput
+import see.day.network.dto.record.exercise.ExerciseRecordEditRequest
 import see.day.network.dto.record.exercise.ExerciseRecordInputRequest
 
-fun ExerciseRecordInput.toDto() : ExerciseRecordInputRequest {
+fun ExerciseRecordInput.toDto(): ExerciseRecordInputRequest {
     return ExerciseRecordInputRequest(
         exerciseType = exerciseType.name,
         dailyNote = dailyNote,
         recordDate = recordDate.formatDate(),
         recordTime = recordDate.formatTime(),
-        caloriesBurned = if(caloriesBurned.isBlank()) null else caloriesBurned.toInt(),
-        exerciseTimeMinutes = if(exerciseTimeMinutes.isBlank()) null else exerciseTimeMinutes.toInt(),
-        stepCount = if(stepCount.isBlank()) null else stepCount.toInt(),
-        weight = if(weight.isBlank()) null else weight.toFloat(),
+        caloriesBurned = if (caloriesBurned.isBlank()) null else caloriesBurned.toInt(),
+        exerciseTimeMinutes = if (exerciseTimeMinutes.isBlank()) null else exerciseTimeMinutes.toInt(),
+        stepCount = if (stepCount.isBlank()) null else stepCount.toInt(),
+        weight = if (weight.isBlank()) null else weight.toFloat(),
         imageUrls = imageUrls
+    )
+}
+
+fun ExerciseRecordEdit.toDto(): ExerciseRecordEditRequest {
+    return ExerciseRecordEditRequest(
+        exerciseType = exerciseType.name,
+        caloriesBurned = if (caloriesBurned.isBlank()) null else caloriesBurned.toIntOrNull(),
+        exerciseTimeMinutes = if (exerciseTimeMinutes.isBlank()) null else exerciseTimeMinutes.toInt(),
+        stepCount = if (stepCount.isBlank()) null else stepCount.toInt(),
+        weight = if (weight.isBlank()) null else weight.toFloat(),
+        dailyNote = dailyNote,
+        imageUrls = imageUrls,
+        recordTime = recordTime
     )
 }
