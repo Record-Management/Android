776176f23196bc2c24db677c0b1a876951d4b9d1
이건희 <leech9876@naver.com>
2025-10-15 00:20:07 +0900

    [ANDROID-86] feat : 운동 기록 수정 endpoint 작성 및 테스트 코드 작성

--- core/data/src/main/java/see/day/network/ExerciseRecordService.kt
+++ core/data/src/main/java/see/day/network/ExerciseRecordService.kt
@@ -2,12 +2,21 @@ package see.day.network
 
 import retrofit2.http.Body
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
 import see.day.network.dto.CommonResponse
 import see.day.network.dto.record.ExerciseRecordResponse
+import see.day.network.dto.record.exercise.ExerciseRecordEditRequest
 import see.day.network.dto.record.exercise.ExerciseRecordInputRequest
 
 interface ExerciseRecordService {
 
     @POST("api/exercise-records")
     suspend fun postExerciseRecord(@Body exerciseRecordInput: ExerciseRecordInputRequest) : CommonResponse<ExerciseRecordResponse>
+
+    @PUT("api/exercise-records/{exerciseRecordId}")
+    suspend fun updateExerciseRecord(
+        @Path("exerciseRecordId") recordId: String,
+        @Body exerciseRecordEdit: ExerciseRecordEditRequest
+    ) : CommonResponse<ExerciseRecordResponse>
 }
--- core/data/src/test/java/see/day/data/api/exerciseRecordService/json/PostExerciseRecord.kt
+++ core/data/src/test/java/see/day/data/api/exerciseRecordService/json/PostExerciseRecord.kt
@@ -112,3 +112,51 @@ val postOverLimitRecordsFailureResponse = """
       "data": null
     }
 """.trimIndent()
+
+val updateExerciseRequestSuccessResponse = """
+    {
+      "code": "S200",
+      "statusCode": 200,
+      "message": "운동기록이 성공적으로 수정되었습니다",
+      "data": {
+        "id": "b6e1b665-e1f3-4e2b-b6c1-efb9b32c7be8",
+        "type": "EXERCISE",
+        "recordDate": [
+          2025,
+          1,
+          7
+        ],
+        "recordTime": [
+          13,
+          30
+        ],
+        "createdAt": [
+          2025,
+          9,
+          18,
+          15,
+          30,
+          0,
+          0
+        ],
+        "updatedAt": [
+          2025,
+          9,
+          18,
+          15,
+          30,
+          0,
+          0
+        ],
+        "exerciseType": "RUNNING",
+        "caloriesBurned": 100,
+        "exerciseTimeMinutes": 100,
+        "stepCount": 100,
+        "weight": 100.5,
+        "dailyNote": "오늘 농구 경기 정말 재밌었다! 팀워크가 좋았어요.",
+        "imageUrls": [
+          "https://seeday-images.s3.ap-northeast-2.amazonaws.com/records/images/basketball-uuid.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Expires=3600&..."
+        ]
+      }
+    }
+""".trimIndent()
