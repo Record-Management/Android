04508996fafe1fcbd9696c4963d4e42832cac87a
이건희 <leech9876@naver.com>
2025-10-19 16:15:52 +0900

    [ANDROID-92] feat : 습관 선택 바텀시트 작성

--- feature/habit/src/main/java/see/day/habit/screen/HabitDetailScreen.kt
+++ feature/habit/src/main/java/see/day/habit/screen/HabitDetailScreen.kt
@@ -5,7 +5,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,6 +18,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import see.day.designsystem.theme.SeeDayTheme
 import see.day.designsystem.util.getIconRes
+import see.day.habit.component.HabitSelectBottomSheet
 import see.day.habit.state.HabitRecordPostType
 import see.day.model.record.RecordType
 import see.day.model.record.habit.HabitType
@@ -23,15 +26,28 @@ import see.day.ui.component.TypeTitle
 import see.day.ui.topbar.DetailRecordTopBar
 import see.day.ui.topbar.EditMode
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun HabitDetailScreenRoot(
     habitRecordPostType: HabitRecordPostType,
 ) {
-    if(habitRecordPostType is HabitRecordPostType.Write) {
+    if (habitRecordPostType is HabitRecordPostType.Write) {
         var habitType by remember { mutableStateOf(habitRecordPostType.habitType) }
+        var openSelectEmotionDialog by remember { mutableStateOf(false) }
+        val sheetState = rememberModalBottomSheetState()
 
+        if (openSelectEmotionDialog) {
+            HabitSelectBottomSheet(
+                sheetState = sheetState,
+                onDismiss = { openSelectEmotionDialog = false },
+                onClickHabit = { newHabitType ->
+                    habitType = newHabitType
+                }
+            )
+        }
         HabitDetailScreen(
-            habitType = habitType
+            habitType = habitType,
+            onClickHabitTitle = { openSelectEmotionDialog = true }
         )
     }
 
@@ -40,7 +56,8 @@ internal fun HabitDetailScreenRoot(
 @Composable
 internal fun HabitDetailScreen(
     modifier: Modifier = Modifier,
-    habitType: HabitType
+    habitType: HabitType,
+    onClickHabitTitle: () -> Unit
 ) {
     Scaffold(
         modifier = modifier.systemBarsPadding(),
@@ -63,7 +80,7 @@ internal fun HabitDetailScreen(
                 modifier = modifier.padding(top = 10.dp),
                 typeIcon = habitType.getIconRes,
                 typeName = habitType.displayName,
-                onClickType = {}
+                onClickType = onClickHabitTitle
             )
         }
     }
@@ -73,6 +90,6 @@ internal fun HabitDetailScreen(
 @Composable
 private fun HabitDetailScreenPreview() {
     SeeDayTheme {
-        HabitDetailScreen(habitType = HabitType.SAVING)
+        HabitDetailScreen(habitType = HabitType.SAVING, onClickHabitTitle = {})
     }
 }
