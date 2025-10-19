8d4121310320943c8e5b0ef0ce0aaf34ad120102
이건희 <leech9876@naver.com>
2025-10-19 15:55:36 +0900

    [ANDROID-92] feat : 습관 상세화면에 들어왔을 때 신규 등록인지 수정인지 구분하도록 설정

--- feature/habit/src/main/java/see/day/habit/screen/HabitDetailScreen.kt
+++ feature/habit/src/main/java/see/day/habit/screen/HabitDetailScreen.kt
@@ -7,11 +7,16 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import see.day.designsystem.theme.SeeDayTheme
 import see.day.designsystem.util.getIconRes
+import see.day.habit.state.HabitRecordPostType
 import see.day.model.record.RecordType
 import see.day.model.record.habit.HabitType
 import see.day.ui.component.TypeTitle
@@ -20,11 +25,16 @@ import see.day.ui.topbar.EditMode
 
 @Composable
 internal fun HabitDetailScreenRoot(
-    habitType: HabitType
+    habitRecordPostType: HabitRecordPostType,
 ) {
-    HabitDetailScreen(
-        habitType = habitType
-    )
+    if(habitRecordPostType is HabitRecordPostType.Write) {
+        var habitType by remember { mutableStateOf(habitRecordPostType.habitType) }
+
+        HabitDetailScreen(
+            habitType = habitType
+        )
+    }
+
 }
 
 @Composable
