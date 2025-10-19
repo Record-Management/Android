0fe57a79bda0f7505b73f677e58525ecc62af05d
이건희 <leech9876@naver.com>
2025-10-19 15:46:57 +0900

    [ANDROID-92] feat : 습관 상세화면에서 습관 아이콘하고 텍스트가 나오도록 설정

--- feature/habit/src/main/java/see/day/habit/screen/HabitDetailScreen.kt
+++ feature/habit/src/main/java/see/day/habit/screen/HabitDetailScreen.kt
@@ -1,23 +1,20 @@
 package see.day.habit.screen
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import see.day.designsystem.theme.SeeDayTheme
 import see.day.designsystem.util.getIconRes
 import see.day.model.record.RecordType
 import see.day.model.record.habit.HabitType
+import see.day.ui.component.TypeTitle
 import see.day.ui.topbar.DetailRecordTopBar
 import see.day.ui.topbar.EditMode
 
@@ -52,13 +49,11 @@ internal fun HabitDetailScreen(
                 .padding(horizontal = 16.dp)
                 .verticalScroll(rememberScrollState())
         ) {
-            Image(
-                painter = painterResource(habitType.getIconRes),
-                contentDescription = "",
-                modifier = Modifier.size(100.dp)
-            )
-            Text(
-                habitType.displayName
+            TypeTitle(
+                modifier = modifier.padding(top = 10.dp),
+                typeIcon = habitType.getIconRes,
+                typeName = habitType.displayName,
+                onClickType = {}
             )
         }
     }
