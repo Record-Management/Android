c5f1208bca09973b9e07357e831cfd0a719cc696
이건희 <leech9876@naver.com>
2025-10-19 20:37:15 +0900

    [ANDROID-95] feat : 습관 상세 화면 UiState 작성

---
+++ core/model/src/main/java/see/day/model/record/habit/HabitRecordInput.kt
@@ -0,0 +1,9 @@
+package see.day.model.record.habit
+
+data class HabitRecordInput(
+    val habitType: HabitType,
+    val notificationEnabled: Boolean,
+    val notificationHour: Int,
+    val notificationMinute: Int,
+    val memo: String
+)
