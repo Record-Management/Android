cdacd409ea8914cfd86e08be2cc056c07a31368e
이건희 <leech9876@naver.com>
2025-10-20 09:09:32 +0900

    [ANDROID-97] feat : 습관 기록 삭제 Service 코드 작성

--- core/data/src/main/java/see/day/network/HabitRecordService.kt
+++ core/data/src/main/java/see/day/network/HabitRecordService.kt
@@ -1,7 +1,9 @@
 package see.day.network
 
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.POST
+import retrofit2.http.Path
 import see.day.network.dto.CommonResponse
 import see.day.network.dto.record.HabitRecordResponse
 import see.day.network.dto.record.habit.HabitRecordInputRequest
@@ -9,5 +11,8 @@ import see.day.network.dto.record.habit.HabitRecordInputRequest
 interface HabitRecordService {
 
     @POST("api/habit-records")
-    suspend fun postHabitRecord(@Body habitRecordInput: HabitRecordInputRequest) : CommonResponse<HabitRecordResponse>
+    suspend fun postHabitRecord(@Body habitRecordInput: HabitRecordInputRequest): CommonResponse<HabitRecordResponse>
+
+    @DELETE("api/habit-records/{habitRecordId}")
+    suspend fun deleteHabitRecord(@Path("habitRecordId") recordId: String): CommonResponse<Unit>
 }
---
+++ core/data/src/test/java/see/day/data/api/habitRecordService/json/DeleteHabit.kt
@@ -0,0 +1,10 @@
+package see.day.data.api.habitRecordService.json
+
+val deleteHabitSuccessResponse = """
+    {
+      "statusCode": 200,
+      "code": "S20000",
+      "message": "정상적으로 처리되었습니다.",
+      "data": null
+    }
+""".trimIndent()
