e65fc8f1c743a9d767d505b62a46a5842509c6a0
이건희 <leech9876@naver.com>
2025-10-22 22:08:55 +0900

    [ANDROID-109] feat : 로그아웃 Dialog부착 및 로직 연동

--- feature/setting/src/main/java/see/day/setting/component/ExtSettingComponent.kt
+++ feature/setting/src/main/java/see/day/setting/component/ExtSettingComponent.kt
@@ -14,6 +14,10 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -26,6 +30,7 @@ import see.day.designsystem.theme.SeeDayTheme
 import see.day.designsystem.theme.gray50
 import see.day.designsystem.theme.gray90
 import see.day.setting.R
+import see.day.ui.dialog.ConfirmDialog
 
 @Composable
 internal fun ExtSettingComponent(
@@ -113,10 +118,25 @@ private fun LogoutComponent(
     modifier: Modifier = Modifier,
     onClickLogout: () -> Unit
 ) {
+    var openLogoutDialog by remember { mutableStateOf(false) }
+
+    if(openLogoutDialog) {
+        ConfirmDialog(
+            title = see.day.ui.R.string.logout_title,
+            body = see.day.ui.R.string.logout_body,
+            cancel = see.day.ui.R.string.logout_cancel,
+            confirm = see.day.ui.R.string.logout_confirm,
+            onDismiss = { openLogoutDialog = false},
+            onClickConfirmButton = {
+                onClickLogout()
+                openLogoutDialog = false
+            }
+        )
+    }
     Row(
         modifier = modifier
             .fillMaxWidth()
-            .clickable { onClickLogout() },
+            .clickable { openLogoutDialog = true },
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
