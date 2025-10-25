c1c72b4e3eb06c1a8731f4473957935e05a2e3e7
이건희 <leech9876@naver.com>
2025-10-25 21:06:01 +0900

    [ANDROID-111] feat : 알림의 TopBar를 공통의 TopBar로 변경 및 알림 화면에도 적용

--- feature/notification/src/main/java/see/day/notification/screen/NotificationScreen.kt
+++ feature/notification/src/main/java/see/day/notification/screen/NotificationScreen.kt
@@ -1,11 +1,16 @@
 package see.day.notification.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import see.day.designsystem.theme.SeeDayTheme
+import see.day.notification.R
+import see.day.ui.topbar.CommonAppBar
 
 @Composable
 internal fun NotificationScreenRoute(
@@ -21,9 +26,25 @@ internal fun NotificationScreen(
     modifier: Modifier = Modifier,
     onBack: () -> Unit
 ) {
-    Column {
-        Text("Notification")
+    Scaffold(
+        modifier = modifier
+            .systemBarsPadding(),
+        topBar = {
+            CommonAppBar(
+                title = R.string.notification_title,
+                onClickBackButton = onBack
+            )
+        }
+    ) { innerPadding ->
+        Column {
+            Text(
+                modifier = Modifier.padding(innerPadding),
+                text = "Notification"
+            )
+
+        }
     }
+
 }
 
 
