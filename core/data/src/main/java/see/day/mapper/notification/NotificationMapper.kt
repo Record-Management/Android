5a755f4a925e6d4c81c6ddefbeebdaed44361f80
이건희 <leech9876@naver.com>
2025-10-27 12:27:32 +0900

    [ANDROID-112] feat : Notification Response를 model 클래스로 변경하는 매퍼 작성

---
+++ core/data/src/main/java/see/day/mapper/notification/NotificationMapper.kt
@@ -0,0 +1,26 @@
+package see.day.mapper.notification
+
+import see.day.model.notification.NotificationHistory
+import see.day.model.notification.NotificationHistoryList
+import see.day.model.record.RecordType
+import see.day.network.dto.notification.NotificationHistoryItemResponse
+import see.day.network.dto.notification.NotificationHistoryResponse
+
+fun NotificationHistoryResponse.toModel() : NotificationHistoryList {
+    return NotificationHistoryList(
+        notifications = this.notifications.items.toModel(),
+        recentCheckedAt = recentCheckedAt
+    )
+}
+
+fun List<NotificationHistoryItemResponse>.toModel() : List<NotificationHistory> {
+    return this.map { it.toModel() }
+}
+
+fun NotificationHistoryItemResponse.toModel() : NotificationHistory {
+    return NotificationHistory(
+        recordType = RecordType.valueOf(mainRecordType),
+        description = description,
+        sentAt = sentAt
+    )
+}
