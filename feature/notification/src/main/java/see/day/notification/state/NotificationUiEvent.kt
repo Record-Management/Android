6c71386c7a717e3530499d230516fd4c1b4d7840
이건희 <leech9876@naver.com>
2025-10-27 14:45:21 +0900

    [ANDROID-112] feat : NotificationUiEvent 작성

---
+++ feature/notification/src/main/java/see/day/notification/state/NotificationUiEvent.kt
@@ -0,0 +1,8 @@
+package see.day.notification.state
+
+import see.day.model.record.RecordType
+
+sealed interface NotificationUiEvent {
+    data object OnClickBack: NotificationUiEvent
+    data class OnClickItem(val recordType: RecordType, val relativeTime: String) : NotificationUiEvent
+}
