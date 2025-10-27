f87e2bd719c81960df9b7fd68413ad792f466ae8
이건희 <leech9876@naver.com>
2025-10-27 10:11:21 +0900

    [ANDROID-111] feat : 시간에서 며칠전 혹은 몇시간 전인지 계산해주는 로직 작성

---
+++ feature/notification/src/main/java/see/day/notification/util/TimeFormatUtil.kt
@@ -0,0 +1,26 @@
+package see.day.notification.util
+
+import java.time.Duration
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+object TimeFormatUtil {
+    fun getRelativeTimeString(dateTimeString: String): String {
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd:HH:mm:ss")
+        val dateTime = LocalDateTime.parse(dateTimeString, formatter)
+        val now = LocalDateTime.now(ZoneId.of("Asia/Seoul"))
+
+        val duration = Duration.between(dateTime, now)
+        val hours = duration.toHours()
+        val days = duration.toDays()
+
+        return when {
+            days >= 1 -> "${days}일 전"
+            hours >= 0 -> "${hours}시간 전"
+            else -> {
+                "0시간 전"
+            }
+        }
+    }
+}
