c4822f13fc41f8cc692bdc36ac4125b3be0e6bd6
이건희 <leech9876@naver.com>
2025-10-28 17:40:11 +0900

    [ANDROID-112] feat : ViewModel 로직 작성, 오늘의 레코드 타입을 받는 UseCase 작성

--- feature/notification/src/main/java/see/day/notification/state/NotificationUiState.kt
+++ feature/notification/src/main/java/see/day/notification/state/NotificationUiState.kt
@@ -1,11 +1,15 @@
 package see.day.notification.state
 
+import see.day.model.calendar.RecordDetail
+import see.day.model.record.RecordType
+
 data class NotificationUiState(
     val recentCheckedAt: String,
     val notificationHistories: List<NotificationHistoryUiModel>,
-    val todayRecordCount: Int
+    val todayRecords: List<RecordDetail>,
+    val mainRecordType: RecordType
 ) {
     companion object {
-        val init = NotificationUiState("", listOf(), 0)
+        val init = NotificationUiState("", listOf(), listOf(), RecordType.DAILY)
     }
 }
--- feature/notification/src/main/java/see/day/notification/viewModel/NotificationViewModel.kt
+++ feature/notification/src/main/java/see/day/notification/viewModel/NotificationViewModel.kt
@@ -11,9 +11,10 @@ import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import see.day.domain.usecase.calendar.GetDailyRecordsCountUseCase
+import see.day.domain.usecase.calendar.GetDailyRecordsUseCase
 import see.day.domain.usecase.notifiaction.GetNotificationHistoryUseCase
 import see.day.domain.usecase.notifiaction.UpdateNotificationHistoryAllReadUseCase
+import see.day.domain.usecase.user.GetMainRecordTypeUseCase
 import see.day.model.record.RecordType
 import see.day.notification.state.NotificationHistoryUiModel
 import see.day.notification.state.NotificationUiEffect
@@ -21,14 +22,17 @@ import see.day.notification.state.NotificationUiEvent
 import see.day.notification.state.NotificationUiState
 import see.day.notification.util.TimeFormatUtil
 import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.ZoneId
 import javax.inject.Inject
+import kotlin.text.format
 
 @HiltViewModel
 class NotificationViewModel @Inject constructor(
     private val getNotificationHistoryUseCase: GetNotificationHistoryUseCase,
     private val updateNotificationHistoryAllReadUseCase: UpdateNotificationHistoryAllReadUseCase,
-    private val getDailyRecordsCountUseCase: GetDailyRecordsCountUseCase
+    private val getDailyRecordsUseCase: GetDailyRecordsUseCase,
+    private val getMainRecordTypeUseCase: GetMainRecordTypeUseCase
 ) : ViewModel() {
 
     private val _uiState: MutableStateFlow<NotificationUiState> = MutableStateFlow(NotificationUiState.init)
@@ -67,13 +71,15 @@ class NotificationViewModel @Inject constructor(
                         updateNotificationHistoryAllReadUseCase()
                     }
 
-                    val todayRecordCount = getDailyRecordsCountUseCase(todayDateString)
+                    val todayRecords = getDailyRecordsUseCase(todayDateString).getOrNull()?.records ?: listOf()
+                    val mainRecordType = getMainRecordTypeUseCase()
 
                     _uiState.update {
                         it.copy(
                             recentCheckedAt = recentCheckedAt,
-                            notificationHistories = uiModel,
-                            todayRecordCount = todayRecordCount
+                            notificationHistories = getDummyHistories(),
+                            todayRecords = todayRecords,
+                            mainRecordType = mainRecordType ?: RecordType.DAILY
                         )
                     }
                 }
@@ -98,23 +104,27 @@ class NotificationViewModel @Inject constructor(
         }
     }
 
+    // 오늘 주 메인 기록 기록이 있으면 그냥 넘어가고 2개 이상 있으면 무조건 넘어가기
     private fun onClickItem(recordType: RecordType, relativeTime: String) {
         viewModelScope.launch {
-            if (relativeTime.contains("시간 전")) {
-                if (uiState.value.todayRecordCount >= 2) {
+            val isTodayNotification = relativeTime.contains("시간 전")
+            val hasAlreadyRecordedToday = uiState.value.todayRecords.size >= 2 ||
+                uiState.value.todayRecords.any { it.type == uiState.value.mainRecordType }
+
+            if (isTodayNotification) {
+                if (hasAlreadyRecordedToday) {
                     _toastMessage.emit("이미 기록을 작성했어요.")
                     _uiEffect.emit(NotificationUiEffect.OnPopBack)
                 } else {
                     _uiEffect.emit(NotificationUiEffect.GoWriteRecord(recordType))
                 }
-            } else if (relativeTime.contains("일 전")) {
-                if (uiState.value.todayRecordCount >= 2) {
+            } else {
+                if (hasAlreadyRecordedToday) {
                     _toastMessage.emit("지나간 기록은 기록할 수 없어요. 내일 또 만나요!")
                     _uiEffect.emit(NotificationUiEffect.OnPopBack)
                 } else {
                     _toastMessage.emit(
-                        "지나간 기록은 기록할 수 없어요. \n" +
-                            "오늘의 기록을 작성해 보는건 어떨까요?"
+                        "지나간 기록은 기록할 수 없어요. 오늘의 기록을 작성해 보는건 어떨까요?"
                     )
                     _uiEffect.emit(NotificationUiEffect.GoWriteRecord(recordType))
                 }
@@ -126,4 +136,34 @@ class NotificationViewModel @Inject constructor(
     private fun LocalDate.toFormattedString(): String {
         return String.format("%04d-%02d-%02d", year, monthValue, dayOfMonth)
     }
+}
+
+
+private fun getDummyHistories() : List<NotificationHistoryUiModel> {
+    return listOf(
+        NotificationHistoryUiModel(
+            recordType = RecordType.DAILY,
+            relativeTime = TimeFormatUtil.getRelativeTimeString(TimeFormatUtil.hourBefore(1L)),
+            isChecked = TimeFormatUtil.hourBefore(1L) < TimeFormatUtil.hourBefore(2L)
+        ),
+        NotificationHistoryUiModel(
+            recordType = RecordType.EXERCISE,
+            relativeTime = TimeFormatUtil.getRelativeTimeString(TimeFormatUtil.hourBefore(3L)),
+            isChecked = TimeFormatUtil.hourBefore(3L) < TimeFormatUtil.hourBefore(2L)
+        ),
+        NotificationHistoryUiModel(
+            recordType = RecordType.HABIT,
+            relativeTime = TimeFormatUtil.getRelativeTimeString(TimeFormatUtil.daysBefore(1L)),
+            isChecked = TimeFormatUtil.daysBefore(1L) < TimeFormatUtil.hourBefore(2L)
+        ),
+        NotificationHistoryUiModel(
+            recordType = RecordType.DAILY,
+            relativeTime = TimeFormatUtil.getRelativeTimeString(TimeFormatUtil.daysBefore(3L)),
+            isChecked = TimeFormatUtil.daysBefore(3L) < TimeFormatUtil.hourBefore(2L)
+        )
+    )
+}
+
+private fun LocalDateTime.toFormattedString(): String {
+    return String.format("%04d-%02d-%02d", year, monthValue, dayOfMonth)
 }
