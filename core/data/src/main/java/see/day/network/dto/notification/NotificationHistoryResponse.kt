1ae4cb0a322d59801072cf978b4a07ba459e02ae
이건희 <leech9876@naver.com>
2025-10-28 17:41:06 +0900

    [ANDROID-112] fix : 알림 히스토리 날짜 Array 형태로 떨어지게 수정

--- core/data/src/main/java/see/day/network/dto/notification/NotificationHistoryResponse.kt
+++ core/data/src/main/java/see/day/network/dto/notification/NotificationHistoryResponse.kt
@@ -1,12 +1,14 @@
 package see.day.network.dto.notification
 
 import kotlinx.serialization.Serializable
+import see.day.network.decoder.FlexibleDateTimeArraySerializer
 import see.day.network.dto.PageInfoResponse
 
 @Serializable
 data class NotificationHistoryResponse(
     val notifications: NotificationHistoryDataResponse,
-    val recentCheckedAt: String?
+    @Serializable(with = FlexibleDateTimeArraySerializer::class)
+    val recentCheckedAt: String? = null
 )
 
 @Serializable
@@ -19,5 +21,6 @@ data class NotificationHistoryDataResponse(
 data class NotificationHistoryItemResponse(
     val mainRecordType: String,
     val description: String,
+    @Serializable(with = FlexibleDateTimeArraySerializer::class)
     val sentAt: String
 )
