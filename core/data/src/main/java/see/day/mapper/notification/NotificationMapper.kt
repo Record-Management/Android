27498816ce52013c1efca93c719aa98821185e9e
이건희 <leech9876@naver.com>
2025-10-29 17:17:35 +0900

    [ANDROID-117] feat : Notification 세팅 모델 클래스 작성, 매퍼함수 작성

--- core/data/src/main/java/see/day/mapper/notification/NotificationMapper.kt
+++ core/data/src/main/java/see/day/mapper/notification/NotificationMapper.kt
@@ -2,25 +2,37 @@ package see.day.mapper.notification
 
 import see.day.model.notification.NotificationHistory
 import see.day.model.notification.NotificationHistoryList
+import see.day.model.notification.NotificationSettings
 import see.day.model.record.RecordType
 import see.day.network.dto.notification.NotificationHistoryItemResponse
 import see.day.network.dto.notification.NotificationHistoryResponse
+import see.day.network.dto.notification.NotificationSettingResponse
 
-fun NotificationHistoryResponse.toModel() : NotificationHistoryList {
+fun NotificationHistoryResponse.toModel(): NotificationHistoryList {
     return NotificationHistoryList(
         notifications = this.notifications.items.toModel(),
         recentCheckedAt = recentCheckedAt
     )
 }
 
-fun List<NotificationHistoryItemResponse>.toModel() : List<NotificationHistory> {
+fun List<NotificationHistoryItemResponse>.toModel(): List<NotificationHistory> {
     return this.map { it.toModel() }
 }
 
-fun NotificationHistoryItemResponse.toModel() : NotificationHistory {
+fun NotificationHistoryItemResponse.toModel(): NotificationHistory {
     return NotificationHistory(
         recordType = RecordType.valueOf(mainRecordType),
         description = description,
         sentAt = sentAt
+    )
+}
+
+fun NotificationSettingResponse.toModel(): NotificationSettings {
+    return NotificationSettings(
+        userId = userId,
+        dailyRecordEnabled = dailyRecordNotificationEnabled,
+        exerciseRecordEnabled = exerciseNotificationEnabled,
+        habitRecordEnabled = habitNotificationEnabled,
+        noGoalsNotificationEnabled = noGoalNotificationEnabled
     )
 }
