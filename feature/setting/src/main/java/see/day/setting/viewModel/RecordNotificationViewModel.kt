1a155baeea45817bc69add01c0f53d0a98c47ce5
이건희 <leech9876@naver.com>
2025-10-29 14:48:07 +0900

    [ANDROID-115] feat : ViewModel 작성

---
+++ feature/setting/src/main/java/see/day/setting/viewModel/RecordNotificationViewModel.kt
@@ -0,0 +1,62 @@
+package see.day.setting.viewModel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import see.day.setting.state.record.RecordNotificationUiEffect
+import see.day.setting.state.record.RecordNotificationUiEvent
+import see.day.setting.state.record.RecordNotificationUiState
+import javax.inject.Inject
+
+@HiltViewModel
+class RecordNotificationViewModel @Inject constructor() : ViewModel() {
+
+    private val _uiState: MutableStateFlow<RecordNotificationUiState> = MutableStateFlow(RecordNotificationUiState.init)
+    val uiState: StateFlow<RecordNotificationUiState> = _uiState.asStateFlow()
+
+    private val _uiEffect: MutableSharedFlow<RecordNotificationUiEffect> = MutableSharedFlow()
+    val uiEffect: SharedFlow<RecordNotificationUiEffect> = _uiEffect.asSharedFlow()
+
+    fun onEvent(uiEvent: RecordNotificationUiEvent) {
+        when (uiEvent) {
+            RecordNotificationUiEvent.OnGoBack -> {
+                onGoBack()
+            }
+            is RecordNotificationUiEvent.OnChangedRecordNotification -> {
+                onChangedRecordNotification(
+                    dailyRecordEnabled = uiEvent.dailyRecordNotificationEnabled,
+                    exerciseRecordEnabled = uiEvent.exerciseRecordNotificationEnabled,
+                    habitRecordEnabled = uiEvent.habitRecordNotificationEnabled
+                )
+            }
+        }
+    }
+
+    private fun onGoBack() {
+        viewModelScope.launch {
+            _uiEffect.emit(RecordNotificationUiEffect.OnGoBack)
+        }
+    }
+
+    private fun onChangedRecordNotification(
+        dailyRecordEnabled: Boolean?,
+        exerciseRecordEnabled: Boolean?,
+        habitRecordEnabled: Boolean?
+    ) {
+        _uiState.update {
+            it.copy(
+                dailyRecordNotificationEnabled = dailyRecordEnabled ?: it.dailyRecordNotificationEnabled,
+                exerciseRecordNotificationEnabled = exerciseRecordEnabled ?: it.exerciseRecordNotificationEnabled,
+                habitRecordNotificationEnabled = habitRecordEnabled ?: it.habitRecordNotificationEnabled
+            )
+        }
+    }
+}
