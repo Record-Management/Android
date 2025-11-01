46c15768ef34976d794618c1b26f9526095814fe
이건희 <leech9876@naver.com>
2025-11-01 21:14:12 +0900

    [ANDROID-119] feat : FirebaseMessageService에 로깅 추가

--- core/messaging/src/main/java/see/day/messaging/SeedayFirebaseMessagingService.kt
+++ core/messaging/src/main/java/see/day/messaging/SeedayFirebaseMessagingService.kt
@@ -2,14 +2,30 @@ package see.day.messaging
 
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import see.day.domain.usecase.user.UpdateFcmTokenUseCase
+import timber.log.Timber
 
-class SeedayFirebaseMessagingService() : FirebaseMessagingService() {
+@AndroidEntryPoint
+class SeedayFirebaseMessagingService(
+    private val updateFcmTokenUseCase: UpdateFcmTokenUseCase
+) : FirebaseMessagingService() {
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
+
+        Timber.i("newToken : $token")
+        CoroutineScope(Dispatchers.IO).launch {
+            updateFcmTokenUseCase(token)
+        }
     }
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
+
+        Timber.i("message : $message")
     }
 }
