9dc7a19567c69643e2f1259937ac7f12866942b2
이건희 <leech9876@naver.com>
2025-11-09 00:34:44 +0900

    [ANDROID-134] feat : 목표 재설정 API 엔드포인트 작성

--- core/data/src/main/java/see/day/network/GoalService.kt
+++ core/data/src/main/java/see/day/network/GoalService.kt
@@ -2,10 +2,14 @@ package see.day.network
 
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
 import see.day.network.dto.CommonResponse
 import see.day.network.dto.goal.CurrentGoalResponse
 
 interface GoalService {
+
+    @POST("api/goals/new")
+    suspend fun postNewGoal(@Header("userId") userId: String) : CommonResponse<Unit>
 
     @GET("api/goals/current")
     suspend fun getCurrentGoal(@Header("userId") userId: String) : CommonResponse<CurrentGoalResponse>
