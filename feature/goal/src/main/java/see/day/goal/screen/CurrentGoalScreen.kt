1032edeb3fc7016ca2dea3e4f171b91da1650927
이건희 <leech9876@naver.com>
2025-11-09 16:40:13 +0900

    [ANDROID-136] feat : 목표 달성 리포트 탑바 연동

--- feature/goal/src/main/java/see/day/goal/screen/CurrentGoalScreen.kt
+++ feature/goal/src/main/java/see/day/goal/screen/CurrentGoalScreen.kt
@@ -1,10 +1,15 @@
 package see.day.goal.screen
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import see.day.designsystem.theme.SeeDayTheme
+import see.day.goal.R
+import see.day.ui.topbar.ClosableTopBar
 
 @Composable
 internal fun CurrentGoalScreenRoot(
@@ -19,11 +24,23 @@ internal fun CurrentGoalScreenRoot(
 
 @Composable
 internal fun CurrentGoalScreen(
+    modifier: Modifier = Modifier,
     onBack: () -> Unit,
     userId: String
 ) {
-    Column {
-        Text(userId)
+    Scaffold(
+        topBar = {
+            ClosableTopBar(
+                modifier = modifier.padding(horizontal = 16.dp),
+                onClose = onBack,
+                title = R.string.goal_acheive_title
+            )
+        }
+    ) { innerPadding ->
+        Text(
+            modifier = Modifier.padding(innerPadding),
+            text = userId
+        )
     }
 }
 
