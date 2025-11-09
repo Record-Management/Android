83be626fe2d466a8da6fc4680cb26a22ab09709d
이건희 <leech9876@naver.com>
2025-11-09 16:57:25 +0900

    [ANDROID-136] feat : UiState 작성

---
+++ feature/goal/src/main/java/see/day/goal/state/CurrentGoalUiState.kt
@@ -0,0 +1,28 @@
+package see.day.goal.state
+
+import see.day.model.goal.TreeStage
+import see.day.model.record.RecordType
+
+data class CurrentGoalUiState(
+    val userId: String,
+    val treeStage: TreeStage,
+    val recordType: RecordType,
+    val startDate: String,
+    val endDate: String,
+    val achievementRate: Int,
+    val completedDays: Int,
+    val dayStreak: Int,
+) {
+    companion object {
+        val init = CurrentGoalUiState(
+            userId = "",
+            treeStage = TreeStage.STAGE_1,
+            recordType = RecordType.DAILY,
+            startDate = "2025-11-01",
+            endDate = "2025-11-10",
+            achievementRate = 10,
+            completedDays = 1,
+            dayStreak = 1
+        )
+    }
+}
