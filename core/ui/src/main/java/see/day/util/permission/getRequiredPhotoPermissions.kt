79beb4d93b8f1a3fd879364ee7f50b146e419e82
이건희 <leech9876@naver.com>
2025-11-12 20:14:32 +0900

    [ANDROID-138] feat : API32 이하에서도 사진이 추가되도록 설정

--- core/ui/src/main/java/see/day/util/permission/getRequiredPhotoPermissions.kt
+++ core/ui/src/main/java/see/day/util/permission/getRequiredPhotoPermissions.kt
@@ -13,11 +13,6 @@ fun requiredPhotoPermissionsPreApi33(): String  {
     return Manifest.permission.READ_EXTERNAL_STORAGE
 }
 
-// API 32 이하에서 권한을 요청하는 함수 작성
-
-// 권한을 요청하고 사진칸으로 이동하는 함수 작성
-// 백스택에서도 API 32 이하에서는 bundle에 Uri들이 있는지 조회해야함
-
 fun hasPhotoPermissions(context: Context): Boolean {
     return requiredPhotoPermissionsPreApi33().let { permission ->
         ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED
@@ -28,7 +23,7 @@ fun isPhotoPickerSupported(): Boolean {
     return Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
 }
 
-fun openAppSettings(context: Context) {
+fun openPhotoSettings(context: Context) {
     val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
         data = Uri.fromParts("package", context.packageName, null)
         flags = Intent.FLAG_ACTIVITY_NEW_TASK
