30487d91d65197309cd5a7e309fc970e2291eb3a
이건희 <leech9876@naver.com>
2025-11-17 13:27:55 +0900

    [ANDROID-137] feat : 최근의 목표레포트를 조회하는 Repository 코드 작성

--- core/data/src/main/java/see/day/repository/GoalRepositoryImpl.kt
+++ core/data/src/main/java/see/day/repository/GoalRepositoryImpl.kt
@@ -1,10 +1,13 @@
 package see.day.repository
 
 import see.day.domain.repository.GoalRepository
+import see.day.mapper.goal.toCurrentGoalReport
 import see.day.mapper.goal.toDto
 import see.day.mapper.goal.toModel
+import see.day.model.exception.NoDataException
 import see.day.model.goal.CurrentGoal
 import see.day.model.goal.NewGoal
+import see.day.model.goal.RecentGoalReport
 import see.day.network.GoalService
 import see.day.utils.ErrorUtils.createResult
 import javax.inject.Inject
@@ -22,6 +25,12 @@ class GoalRepositoryImpl @Inject constructor(
     override suspend fun getCurrentGoal(): Result<CurrentGoal?> {
         return createResult {
             goalService.getCurrentGoal().data?.toModel()
+        }
+    }
+
+    override suspend fun getRecentGoalReport(): Result<RecentGoalReport> {
+        return createResult {
+            goalService.getGoalReport().data?.toCurrentGoalReport() ?: throw NoDataException()
         }
     }
 }
--- core/domain/src/main/java/see/day/domain/repository/GoalRepository.kt
+++ core/domain/src/main/java/see/day/domain/repository/GoalRepository.kt
@@ -2,10 +2,13 @@ package see.day.domain.repository
 
 import see.day.model.goal.CurrentGoal
 import see.day.model.goal.NewGoal
+import see.day.model.goal.RecentGoalReport
 
 interface GoalRepository {
 
     suspend fun postNewGoal(newGoal: NewGoal): Result<Unit>
 
     suspend fun getCurrentGoal(): Result<CurrentGoal?>
+
+    suspend fun getRecentGoalReport() : Result<RecentGoalReport>
 }
