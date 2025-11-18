bfefecd6a694cbdc9ea597661088640fdaaef36b
이건희 <leech9876@naver.com>
2025-11-18 16:41:23 +0900

    [ANDROID-154] feat : 목표 재설정 UiState 작성
    필요한 이미지 파일도 미리 옮겼다

---
+++ feature/goal/src/main/java/see/day/goal/state/reset/ResetGoalUiState.kt
@@ -0,0 +1,18 @@
+package see.day.goal.state.reset
+
+import see.day.model.record.RecordType
+
+data class ResetGoalUiState(
+    val step: GoalResetStep,
+    val recordType: RecordType,
+    val goalDays: Int
+) {
+    companion object {
+        val init = ResetGoalUiState(GoalResetStep.RECORD,RecordType.HABIT, goalDays = 10)
+    }
+}
+
+enum class GoalResetStep(val titleRes: Int, val iconRes: Int) {
+    RECORD(see.day.ui.R.string.record_message, see.day.ui.R.drawable.onboard_record),
+    DAY(see.day.ui.R.string.goals_message, see.day.ui.R.drawable.onboard_goal)
+}
