3a8d96d872afc26be63d195d89d1a8c9ea2f89a6
이건희 <leech9876@naver.com>
2025-11-22 01:24:05 +0900

    [ANDROID-42] build : release를 위한 SingingConfig 작성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
 plugins {
     id("youth.android.application")
     id("com.google.gms.google-services")
@@ -14,6 +16,30 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
+    }
+
+    fun getApiKey(propertyKey: String): String {
+        return gradleLocalProperties(rootDir, providers).getProperty(propertyKey)
+    }
+
+    signingConfigs {
+        create("release") {
+            storeFile = file(getApiKey("keystore.file"))
+            storePassword = getApiKey("keystore.password")
+            keyAlias = getApiKey("key.alias")
+            keyPassword = getApiKey("key.password")
+        }
+
+    }
+    buildTypes {
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+    android {
+        lint {
+            disable += "NullSafeMutableLiveData"
+        }
     }
 }
 
