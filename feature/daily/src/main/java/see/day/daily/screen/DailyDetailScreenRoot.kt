197481663e5ac94583830e7927a33777762b4685
이건희 <leech9876@naver.com>
2025-12-17 00:51:13 +0900

    [QA-18] feat : 하루 기록 완료 버튼이 키보드가 나왔을때 키보드 위로 뜨도록 슬정

--- feature/daily/src/main/java/see/day/daily/screen/DailyDetailScreenRoot.kt
+++ feature/daily/src/main/java/see/day/daily/screen/DailyDetailScreenRoot.kt
@@ -4,9 +4,13 @@ import android.widget.Toast
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -46,6 +50,7 @@ import see.day.ui.photo.RecordDetailPhotoRow
 import see.day.ui.textField.RecordWriteTextField
 import see.day.ui.topbar.DetailRecordTopBar
 import see.day.ui.topbar.EditMode
+import see.day.util.advancedImePadding
 
 @Composable
 internal fun DailyDetailScreenRoot(modifier: Modifier = Modifier, viewModel: DailyDetailViewModel = hiltViewModel(), dailyRecordPostType: DailyRecordPostType, onClickPopHome: (Boolean) -> Unit) {
@@ -157,8 +162,8 @@ internal fun DailyDetailScreen(modifier: Modifier = Modifier, uiState: DailyDeta
         modifier = modifier
             .background(Color.White)
             .padding(horizontal = 16.dp)
-            .systemBarsPadding()
-            .imePadding(),
+            .statusBarsPadding()
+            .advancedImePadding(),
         topBar = {
             DetailRecordTopBar(
                 modifier = Modifier,
@@ -175,7 +180,9 @@ internal fun DailyDetailScreen(modifier: Modifier = Modifier, uiState: DailyDeta
         },
         bottomBar = {
             CompleteButton(
-                modifier = Modifier,
+                modifier = Modifier.windowInsetsPadding(
+                    WindowInsets.systemBars.only(WindowInsetsSides.Bottom)
+                ),
                 text = stringResource(
                     when (uiState.editMode) {
                         is DailyDetailUiState.EditMode.Create -> {
