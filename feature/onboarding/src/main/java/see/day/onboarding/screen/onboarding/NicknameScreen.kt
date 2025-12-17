4594cb70476e1c96f792647e4de9152d4cbdae3f
이건희 <57277631+hegunhee@users.noreply.github.com>
2025-12-17 16:22:10 +0900

    [QA-11] 그래픽 품질 이슈 (#167)
    * [QA-11] fix: 하루기록 관련 감정들 이미지 svg로 변경
    
    * [QA-11] fix: 운동기록 관련 운동들 이미지 svg로 변경
    
    * [QA-11] fix: 목표 관련 이미지 svg로 변경
    
    * [QA-11] fix: 습관기록 관련 습관들 이미지 svg로 변경
    
    * [QA-11] fix: 홈 화면 관련 이미지 svg로 변경
    
    * [QA-11] fix: 알림 화면 관련 이미지 svg로 변경
    
    notification 아이콘들 확인하는 Preview, 테스트코드도 수정했음
    
    * [QA-11] fix: 온보딩 화면 관련 이미지 svg로 변경
    
    이미지들은 svg로 다운받지못하기때문에 4x png로 다운받았다
    
    * [QA-11] fix: 설정 화면 관련 이미지 svg로 변경
    
    * [QA-11] fix: 전반적으로 사용하는 이미지들 png로 변경(이미지이기떄문에 png는 불가능)
    
    * [QA-11] fix: 하단 버튼이 네비게이션바에 가리는 이슈 수정

--- feature/onboarding/src/main/java/see/day/onboarding/screen/onboarding/NicknameScreen.kt
+++ feature/onboarding/src/main/java/see/day/onboarding/screen/onboarding/NicknameScreen.kt
@@ -3,6 +3,7 @@ package see.day.onboarding.screen.onboarding
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -31,7 +32,7 @@ internal fun NicknameScreen(modifier: Modifier = Modifier, nickname: String, onC
         Spacer(modifier = modifier.weight(1f))
 
         CompleteButton(
-            modifier = modifier,
+            modifier = modifier.imePadding(),
             text = "다음",
             isEnabled = !isError && nicknameValue.isNotEmpty(),
             onClick = { onComplete(OnboardingUiEvent.EnterNickname(nicknameValue)) }
