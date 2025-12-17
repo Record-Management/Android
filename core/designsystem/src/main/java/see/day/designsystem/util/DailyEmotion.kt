9964b266560cb79042cc7c49cec7956c1ed084cc
이건희 <leech9876@naver.com>
2025-12-17 14:30:24 +0900

    [QA-11] fix: 하루기록 관련 감정들 이미지 svg로 변경

--- core/designsystem/src/main/java/see/day/designsystem/util/DailyEmotion.kt
+++ core/designsystem/src/main/java/see/day/designsystem/util/DailyEmotion.kt
@@ -5,13 +5,13 @@ import see.day.model.record.daily.DailyEmotion
 
 val DailyEmotion.largeIconRes: Int
     get() = when (this) {
-        DailyEmotion.Normal -> R.drawable.emotion_normal_large
-        DailyEmotion.Happy -> R.drawable.emotion_happy_large
-        DailyEmotion.Peaceful -> R.drawable.emotion_peaceful_large
-        DailyEmotion.Funny -> R.drawable.emotion_funny_large
-        DailyEmotion.Love -> R.drawable.emotion_love_large
-        DailyEmotion.Tired -> R.drawable.emotion_tired_large
-        DailyEmotion.Panic -> R.drawable.emotion_panic_large
-        DailyEmotion.Sad -> R.drawable.emotion_sad_large
-        DailyEmotion.Angry -> R.drawable.emotion_angry_large
+        DailyEmotion.Normal -> R.drawable.emotion_normal
+        DailyEmotion.Happy -> R.drawable.emotion_happy
+        DailyEmotion.Peaceful -> R.drawable.emotion_peaceful
+        DailyEmotion.Funny -> R.drawable.emotion_funny
+        DailyEmotion.Love -> R.drawable.emotion_love
+        DailyEmotion.Tired -> R.drawable.emotion_tired
+        DailyEmotion.Panic -> R.drawable.emotion_panic
+        DailyEmotion.Sad -> R.drawable.emotion_sad
+        DailyEmotion.Angry -> R.drawable.emotion_angry
     }
