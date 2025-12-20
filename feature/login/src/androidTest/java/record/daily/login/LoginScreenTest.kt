444053c0d886a65ad21dbae752c285d8ad4d1fd8
이건희 <leech9876@naver.com>
2025-12-20 23:41:29 +0900

    [ANDROID-182] test: 로그인 화면 테스트 코드 작성

---
+++ feature/login/src/androidTest/java/record/daily/login/LoginScreenTest.kt
@@ -0,0 +1,41 @@
+package record.daily.login
+
+import android.content.Context
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import org.junit.Rule
+import org.junit.Test
+import record.daily.login.screen.LoginScreen
+import see.day.designsystem.theme.SeeDayTheme
+
+class LoginScreenTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun given_when_shownSplashImage_kakaoLogin() {
+        // given & when
+        composeTestRule.setContent {
+            SeeDayTheme {
+                LoginScreen(onClickKakaoLogin = {})
+            }
+        }
+
+        // shown
+        composeTestRule
+            .onNodeWithContentDescription("splash_title")
+            .assertIsDisplayed()
+
+        composeTestRule
+            .onNodeWithContentDescription("splash_image")
+            .assertIsDisplayed()
+
+        composeTestRule
+            .onNodeWithContentDescription("kakao_login")
+            .assertIsDisplayed()
+
+    }
+}
