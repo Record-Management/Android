646f79d4fc839a13a181e96356aa97330ff15f0c
이건희 <leech9876@naver.com>
2025-12-20 23:21:49 +0900

    [ANDROID-182] style: Login 모듈 ViewModel, UiState 들여쓰기 수정

--- feature/login/src/main/java/record/daily/login/state/permission/PermissionUiEffect.kt
+++ feature/login/src/main/java/record/daily/login/state/permission/PermissionUiEffect.kt
@@ -1,5 +1,5 @@
 package record.daily.login.state.permission
 
 sealed interface PermissionUiEffect {
-    data object OnGoLogin: PermissionUiEffect
+    data object OnGoLogin : PermissionUiEffect
 }
