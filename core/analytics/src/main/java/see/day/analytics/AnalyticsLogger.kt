c55a2aaf34960085c3fb1663a5aa85ee722a58fc
이건희 <leech9876@naver.com>
2026-01-03 21:36:53 +0900

    [ANDROID-199] feat : 온보딩 완료, 목표 재설정 로깅 코드 작성

--- core/analytics/src/main/java/see/day/analytics/AnalyticsLogger.kt
+++ core/analytics/src/main/java/see/day/analytics/AnalyticsLogger.kt
@@ -4,6 +4,7 @@ import com.google.firebase.Firebase
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.analytics
 import com.google.firebase.analytics.logEvent
+import see.day.analytics.types.GoalSettingType
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -14,7 +15,6 @@ class AnalyticsLogger @Inject constructor(){
     fun log(event: AnalyticsEvent) {
         if(!BuildConfig.DEBUG) {
             firebaseAnalytics.logEvent(event.name) {
-
             }
         }
     }
@@ -23,6 +23,15 @@ class AnalyticsLogger @Inject constructor(){
         if(!BuildConfig.DEBUG) {
             firebaseAnalytics.logEvent(event.name) {
 
+            }
+        }
+    }
+
+    fun goalSettingLog(goalSettingType: GoalSettingType, recordType: String, goalDays: Int) {
+        if(!BuildConfig.DEBUG) {
+            firebaseAnalytics.logEvent(goalSettingType.name.lowercase()) {
+                param("recordType", recordType)
+                param("goalDays", goalDays.toString())
             }
         }
     }
---
+++ core/analytics/src/main/java/see/day/analytics/types/GoalSettingType.kt
@@ -0,0 +1,5 @@
+package see.day.analytics.types
+
+enum class GoalSettingType {
+    ONBOARDING, GOAL_RESET
+}
