8e6d0c01f6933d92e2d774abbbb274b2ad5714ed
이건희 <leech9876@naver.com>
2026-03-30 21:36:46 +0900

    Revert "[ANDROID-213] build : 홈 모듈에 인앱리뷰 의존성 추가"
    This reverts commit 380c6cb1cecf84c27821936dc95add12d88e9141.

--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -10,7 +10,4 @@ dependencies {
     implementation(project(":core:analytics"))
     implementation(libs.balloon)
     implementation(libs.balloon.compose)
-
-    implementation(libs.inapp.review)
-    implementation(libs.inapp.review.ktx)
 }
