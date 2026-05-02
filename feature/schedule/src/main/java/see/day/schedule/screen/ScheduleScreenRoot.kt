78345831d52ee4d1df4c61e6d5fc20f232f395e9
이건희 <57277631+hegunhee@users.noreply.github.com>
2026-05-02 10:49:09 +0900

    [ANDROID-233] feat: 색상 팔레트 바텀 시트 작업 (#219)
    * [ANDROID-233] feat: 색상에 사용되는 string정의
    
    * [ANDROID-233] feat: 색상 팔레트 바텀 시트 작업
    
    * [ANDROID-233] feat: 일정 설정 화면에서 색상 테스트 가능하도록 설정
    
    * [ANDROID-233] feat: 누락된 텍스트 추가

--- feature/schedule/src/main/java/see/day/schedule/screen/ScheduleScreenRoot.kt
+++ feature/schedule/src/main/java/see/day/schedule/screen/ScheduleScreenRoot.kt
@@ -1,20 +1,33 @@
 package see.day.schedule.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import see.day.designsystem.theme.SeeDayTheme
+import see.day.designsystem.theme.primaryColor
+import see.day.schedule.R
 import see.day.schedule.component.AlertBottomSheet
 import see.day.schedule.component.AlertTime
+import see.day.schedule.component.ColorPaletteBottomSheet
 import see.day.schedule.component.RepeatEndTime
 import see.day.schedule.component.RepeatTime
 import see.day.schedule.component.RepeatTimeBottomSheet
@@ -29,8 +42,11 @@ fun ScheduleScreenRoot(onBack: () -> Unit, onClickPopHome: (Boolean) -> Unit) {
     var checkedRepeatTime by remember { mutableStateOf(RepeatTime.NO) }
     var checkedRepeatEndTime by remember { mutableStateOf<RepeatEndTime?>(null) }
 
+    var showColorPaletteBottomSheet by remember { mutableStateOf(false) }
+    var checkedColor by remember { mutableStateOf(primaryColor) }
+
     if (showAlertBottomSheet) {
-        AlertBottomSheet (
+        AlertBottomSheet(
             onDismiss = {
                 showAlertBottomSheet = false
             },
@@ -53,6 +69,17 @@ fun ScheduleScreenRoot(onBack: () -> Unit, onClickPopHome: (Boolean) -> Unit) {
             }
         )
     }
+
+    if (showColorPaletteBottomSheet) {
+        ColorPaletteBottomSheet(
+            selectedColor = checkedColor,
+            onDismiss = {
+                showColorPaletteBottomSheet = false
+            },
+            onColorSelected = { checkedColor = it }
+        )
+    }
+
     Column(
         modifier = Modifier.systemBarsPadding()
     ) {
@@ -76,6 +103,21 @@ fun ScheduleScreenRoot(onBack: () -> Unit, onClickPopHome: (Boolean) -> Unit) {
         Text("반복 시간")
         Text(stringResource(checkedRepeatTime.textRes))
         Text("반복 종료일 : ${checkedRepeatEndTime?.dateStr ?: ""}")
+
+        Button(
+            onClick = {
+                showColorPaletteBottomSheet = true
+            }
+        ) {
+            Text("색상")
+        }
+
+        Box(
+            modifier = Modifier
+                .size(40.dp)
+                .background(color = checkedColor, shape = CircleShape)
+        ) {
+        }
     }
 }
 
