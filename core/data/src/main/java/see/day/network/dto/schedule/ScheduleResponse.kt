296792e1c7263c3232c2828ce96cad9b2c36c4f7
이건희 <leech9876@naver.com>
2026-05-19 21:13:38 +0900

    [ANDROID-245] feat: 일정 반환값 작성

---
+++ core/data/src/main/java/see/day/network/dto/schedule/ScheduleResponse.kt
@@ -0,0 +1,27 @@
+package see.day.network.dto.schedule
+
+import kotlinx.serialization.Serializable
+import see.day.model.schedule.AlertTime
+import see.day.model.schedule.RepeatTime
+import see.day.model.schedule.SchedulePaletteColor
+import see.day.network.decoder.LocalDateSerializer
+import java.time.LocalDate
+
+@Serializable
+data class ScheduleResponse(
+    val scheduleRecordId: String,
+    val title: String,
+    @Serializable(with = LocalDateSerializer::class)
+    val startDate: LocalDate,
+    @Serializable(with = LocalDateSerializer::class)
+    val endDate: LocalDate,
+    val notificationType: AlertTime,
+    val notificationCustomHours: Int? = null,
+    val notificationCustomMinutes: Int? = null,
+    val repeatType: RepeatTime,
+    @Serializable(with = LocalDateSerializer::class)
+    val repeatEndsOn: LocalDate? = null,
+    val location: String,
+    val color: SchedulePaletteColor,
+    val memo: String,
+)
