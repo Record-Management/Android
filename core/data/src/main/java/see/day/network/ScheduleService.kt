efcfec0ec5cbfc1607a3579fa7317d012fd4c2b5
이건희 <leech9876@naver.com>
2026-05-19 21:14:16 +0900

    [ANDROID-245] feat : 일정 요청 api 작성

---
+++ core/data/src/main/java/see/day/network/ScheduleService.kt
@@ -0,0 +1,13 @@
+package see.day.network
+
+import retrofit2.http.Body
+import retrofit2.http.POST
+import see.day.network.dto.schedule.ScheduleInputRequest
+
+interface ScheduleService {
+
+    @POST("/api/schedule-records")
+    suspend fun postSchedule(
+        @Body scheduleInputRequest: ScheduleInputRequest
+    )
+}
