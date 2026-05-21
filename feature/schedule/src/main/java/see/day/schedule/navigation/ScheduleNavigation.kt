46ca0232be9fa3d9b95cc9a93a50917e0942ac32
이건희 <leech9876@naver.com>
2026-05-22 00:46:56 +0900

    [ANDROID-243] feat: 루트 변경

--- feature/schedule/src/main/java/see/day/schedule/navigation/ScheduleNavigation.kt
+++ feature/schedule/src/main/java/see/day/schedule/navigation/ScheduleNavigation.kt
@@ -4,17 +4,18 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
-import see.day.navigation.schedule.ScheduleRoute.Schedule
+import see.day.navigation.schedule.ScheduleRoute.ScheduleWrite
 import see.day.schedule.screen.ScheduleDetailScreenRoot
+import see.day.schedule.state.SchedulePostType
 
 fun NavController.navigateSchedule(navOptions: NavOptions? = null) {
-    navigate(Schedule, navOptions)
+    navigate(ScheduleWrite, navOptions)
 }
 
 fun NavGraphBuilder.scheduleNavigation(onBack: () -> Unit, onClickPopHome: (Boolean) -> Unit) {
-    composable<Schedule> {
+    composable<ScheduleWrite> {
         ScheduleDetailScreenRoot(
-            onBack = onBack,
+            editType = SchedulePostType.Write,
             onClickPopHome = onClickPopHome
         )
     }
