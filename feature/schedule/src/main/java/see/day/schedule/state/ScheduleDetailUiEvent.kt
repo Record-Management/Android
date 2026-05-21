a29ebd344d1775f5e6a5a6a3abc9d05bda7f3597
이건희 <leech9876@naver.com>
2026-05-21 23:20:18 +0900

    [ANDROID-243] fix: 색상 변경 파라미터 수정

--- feature/schedule/src/main/java/see/day/schedule/state/ScheduleDetailUiEvent.kt
+++ feature/schedule/src/main/java/see/day/schedule/state/ScheduleDetailUiEvent.kt
@@ -1,8 +1,8 @@
 package see.day.schedule.state
 
-import androidx.compose.ui.graphics.Color
 import see.day.model.schedule.AlertTime
 import see.day.model.schedule.RepeatTime
+import see.day.model.schedule.SchedulePaletteColor
 import java.time.LocalDate
 
 sealed interface ScheduleDetailUiEvent {
@@ -12,6 +12,6 @@ sealed interface ScheduleDetailUiEvent {
     data class OnAlertTimeChanged(val alertTime: AlertTime, val hour: Int, val minute: Int) : ScheduleDetailUiEvent
     data class OnRepeatTypeChanged(val repeatTime: RepeatTime, val repeatEndTime: LocalDate?) : ScheduleDetailUiEvent
     data class OnLocationChanged(val location: String) : ScheduleDetailUiEvent
-    data class OnColorChanged(val color: Color) : ScheduleDetailUiEvent
+    data class OnColorChanged(val color: SchedulePaletteColor) : ScheduleDetailUiEvent
     data class OnMemoChanged(val memo: String) : ScheduleDetailUiEvent
 }
