3431302d812e3dc599b23b3ae155846bbcc67c11
이건희 <leech9876@naver.com>
2026-05-23 15:27:35 +0900

    [ANDROID-246] feat: RecordType에 스케줄 추가

--- core/model/src/main/java/see/day/model/record/RecordType.kt
+++ core/model/src/main/java/see/day/model/record/RecordType.kt
@@ -1,5 +1,26 @@
 package see.day.model.record
 
-enum class RecordType(val title: String) {
-    DAILY("하루 기록"), EXERCISE("운동 기록"), HABIT("습관 기록"),
+enum class RecordType(
+    val title: String,
+    val isAvailableForGoal: Boolean = true,
+    val isAvailableForMainDisplay: Boolean = true
+) {
+    DAILY("하루 기록"),
+    EXERCISE("운동 기록"),
+    HABIT("습관 기록"),
+    SCHEDULE("일정", isAvailableForGoal = false, isAvailableForMainDisplay = false);
+
+    companion object {
+        fun getGoalAvailableTypes(): List<RecordType> {
+            return entries.filter { it.isAvailableForGoal }
+        }
+
+        fun getMainDisplayAvailableTypes(): List<RecordType> {
+            return entries.filter { it.isAvailableForMainDisplay }
+        }
+
+        fun getCalendarAvailableTypes(): List<RecordType> {
+            return entries.toList()
+        }
+    }
 }
--- feature/home/src/main/java/see/day/home/util/RecordFilterType.kt
+++ feature/home/src/main/java/see/day/home/util/RecordFilterType.kt
@@ -7,6 +7,6 @@ enum class RecordFilterType(@DrawableRes val iconRes: Int) {
     ALL(R.drawable.filter_all),
     DAILY(R.drawable.filter_daily),
     EXERCISE(R.drawable.filter_exercise),
-//    SCHEDULE(R.drawable.filter_schedule),
-    HABIT(R.drawable.filter_habit)
+    HABIT(R.drawable.filter_habit),
+    SCHEDULE(R.drawable.filter_schedule),
 }
