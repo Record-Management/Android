f2a2e8d5c09c18dff06d959090fd614ef7d8042e
이건희 <leech9876@naver.com>
2026-05-24 14:37:50 +0900

    [ANDROID-249] feat: 일정 제한 레포지토리, 매퍼 작성

--- core/data/src/main/java/see/day/network/dto/record/RecordsLimitResponse.kt
+++ core/data/src/main/java/see/day/network/dto/record/RecordsLimitResponse.kt
@@ -1,5 +1,8 @@
 package see.day.network.dto.record
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class RecordsLimitResponse(
     val canCreateRecord: Boolean,
     val canCreateSchedule: Boolean,
