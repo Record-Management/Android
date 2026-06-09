90279f8ad8defb3469c24c5beff115c62abea3a3
이건희 <leech9876@naver.com>
2026-06-09 22:00:39 +0900

    feat: 버전 1.1.0으로 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "see.day.app"
         minSdk = 28
         targetSdk = 35
-        versionCode = 17
-        versionName = "1.0.5"
+        versionCode = 20
+        versionName = "1.1.0"
     }
 
     fun getApiKey(propertyKey: String): String {
